5e5860e797550b80bd44c32ca5925d2c50539e29
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2013-08-18 23:47:13 +0000

    tried a custom route, but it doesn't work yet
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@37 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Login",
+                "-/auth/login",
+                new { controller = "Auth", action = "Login" }
+            );
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
