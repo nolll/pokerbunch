9574153884c5b0a12a6c32fa303e4864f9f0a1ac
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2013-09-06 23:01:27 +0000

    Added Cashgame Listing Item model
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@88 fbe6c1e3-6fed-8744-b610-9659f20f9f49

---
+++ Web/Models/CashgameModels/Listing/CashgameTableItemModel.cs
@@ -0,0 +1,53 @@
+using System;
+using Core.Classes;
+using Infrastructure.System;
+using Web.Models.UrlModels;
+
+namespace Web.Models.CashgameModels.Listing{
+
+	public class CashgameTableItemModel{
+
+	    public int PlayerCount { get; set; }
+		public string Location { get; set; }
+		public string Duration { get; set; }
+		public string Turnover { get; set; }
+		public string AvgBuyin { get; set; }
+		public UrlModel DetailsUrl { get; set; }
+		public string DisplayDate { get; set; }
+		public string PublishedClass { get; set; }
+
+		public CashgameTableItemModel(Homegame homegame, Cashgame cashgame, bool showYear){
+			var playerCount = cashgame.PlayerCount;
+			PlayerCount = playerCount;
+			Location = cashgame.Location;
+			Duration = GetDuration(cashgame);
+			Turnover = GetTurnover(homegame, cashgame);
+			AvgBuyin = GetAvgBuyin(homegame, cashgame, playerCount);
+			DetailsUrl = new CashgameDetailsUrlModel(homegame, cashgame);
+			DisplayDate = Globalization.FormatShortDate(cashgame.StartTime.Value, showYear);
+			PublishedClass = GetPublishedClass(cashgame);
+		}
+
+		private string GetDuration(Cashgame cashgame){
+			var duration = cashgame.Duration;
+			if(duration > 0){
+				return Globalization.FormatDuration(duration);
+			}
+			return string.Empty;
+		}
+
+		private string GetTurnover(Homegame homegame, Cashgame cashgame){
+			return Globalization.FormatCurrency(homegame.Currency, cashgame.Turnover);
+		}
+
+		private string GetAvgBuyin(Homegame homegame, Cashgame cashgame, int playerCount){
+			return Globalization.FormatCurrency(homegame.Currency, cashgame.AverageBuyin);
+		}
+
+		private string GetPublishedClass(Cashgame cashgame){
+			return cashgame.Status == GameStatus.Published ? string.Empty : "unpublished";
+		}
+
+	}
+
+}
