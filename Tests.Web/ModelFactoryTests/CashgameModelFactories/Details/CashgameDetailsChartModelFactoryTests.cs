ee50be017863b0a41abfea6e042a9da8c6cf98aa
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2014-02-18 22:55:56 +0000

    Added a test to CashgameDetailsChartModelFactory. Will add more later. There is a bug here somewhere that mixes players results up
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@426 fbe6c1e3-6fed-8744-b610-9659f20f9f49

---
+++ Tests.Web/ModelFactoryTests/CashgameModelFactories/Details/CashgameDetailsChartModelFactoryTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Application.Services;
+using Core.Classes;
+using Core.Repositories;
+using NUnit.Framework;
+using Tests.Common;
+using Tests.Common.FakeClasses;
+using Web.ModelFactories.CashgameModelFactories.Details;
+using Web.ModelFactories.ChartModelFactories;
+
+namespace Tests.Web.ModelFactoryTests.CashgameModelFactories.Details
+{
+    public class CashgameDetailsChartModelFactoryTests : MockContainer
+    {
+        [Test]
+        public void Create()
+        {
+            const int playerId1 = 1;
+            const int playerId2 = 2;
+
+            var homegame = new FakeHomegame();
+            var cashgame = new FakeCashgame();
+            var player1 = new FakePlayer(id: playerId1);
+            var player2 = new FakePlayer(id: playerId2);
+            var playerList = new List<Player> {player1, player2};
+
+            GetMock<ICashgameService>().Setup(o => o.GetPlayers(cashgame)).Returns(playerList);
+
+            var sut = GetSut();
+            var result = sut.Create(homegame, cashgame);
+
+            Assert.NotNull(result);
+        }
+
+        private CashgameDetailsChartModelFactory GetSut()
+        {
+            return new CashgameDetailsChartModelFactory(
+                GetMock<ITimeProvider>().Object,
+                GetMock<IChartValueModelFactory>().Object,
+                GetMock<ICashgameService>().Object);
+        }
+    }
+}
