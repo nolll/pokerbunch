07dc495c88ae1d3b26799b65ce8fe2f5af76c7cb
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2014-10-22 22:12:36 +0000

    Deleted unused projects
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@718 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Infrastructure.Data/Cache/CacheTime.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infrastructure.Data.Cache
-{
-    public static class CacheTime
-    {
-        public const int Short = 10;
-        public const int Medium = 120;
-        public const int Long = 1440;
-    }
-}
