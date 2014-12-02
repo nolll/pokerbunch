66badd58c798ed5d1302912e6e46e4fa180f3997
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2014-12-02 00:13:48 +0000

    Got the chart working!
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/branches/knockout@764 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Web/FrontEnd/js/knockout-extended.js
+++ Web/FrontEnd/js/knockout-extended.js
@@ -1,5 +1,107 @@
 define([
-    'knockout-raw'
-], function (ko) {
+    'knockout-raw', 'linechart2'
+], function (ko, lineChart) {
+    ko.bindingHandlers.gameChart = {
+        init: function(element) {
+            var config = {
+                pointSize: 0,
+                vAxis: { minValue: 0 },
+                hAxis: { format: 'HH:mm' }
+            };
+            var chart = lineChart.init(element, config);
+            ko.utils.domData.set(element, 'chart', chart);
+        },
+        update: function (element, valueAccessor) {
+            var players = valueAccessor();
+            var data = getGameChartData(players());
+            var chart = ko.utils.domData.get(element, 'chart');
+            chart.draw(data);
+        }
+    };
+
+    function getGameChartData(players) {
+        var data = {
+            cols: getColumns(players),
+            rows: getRows(players),
+            p: null
+        };
+        return data;
+    }
+
+    function getColumns(players) {
+        var i, p,
+            cols = [{ "type": "datetime", "label": "Time", "pattern": "HH:mm" }];
+        for(i = 0; i < players.length; i++) {
+            p = players[i];
+            cols.push({ "type": "number", "label": p.name, "pattern": null });
+        }
+        return cols;
+    }
+
+    function getRows(players) {
+        var i, j, p, r;
+        var rows = [];
+        for(i = 0; i < players.length; i++) {
+            p = players[i];
+            r = getPlayerResults(p);
+            for(j = 0; j < r.length; j++) {
+                rows.push(getRow(players, r[j], p.id));
+            }
+        }
+        return rows;
+    }
+
+    function getPlayerResults(player) {
+        var i,
+            c,
+            current = 0,
+            results = [],
+            checkpoints = player.checkpoints();
+        for (i = 0; i < checkpoints.length; i++) {
+            c = checkpoints[i];
+            current = c.stack - c.addedMoney;
+            results.push({time: c.time, winnings: current});
+        }
+        return results;
+    }
+
+    function getRow(players, result, playerId) {
+        var values = [{ 'v': result.time.toDate(), "f": null }];
+        for(var i = 0; i < players.length; i++) {
+            var val = null;
+            if (players[i].id == playerId)
+            {
+                val = result.winnings + "";
+            }
+            values.push({ 'v': val, "f": null });
+        }
+        return { 'c': values };
+    }
+
     return ko;
 });
+
+
+//"cols":
+//    [
+//        { "type": "datetime", "label": "Time", "pattern": "HH:mm" },
+//        { "type": "number", "label": "Henrik S", "pattern": null }
+//    ],
+//"rows":
+//    [
+//        {
+//            "c":
+//              [
+//                  { "v": "Date(2014, 12, 1, 21, 58, 5)", "f": null },
+//                  { "v": "0", "f": null }
+//              ]
+//        },
+//        {
+//            "c":
+//              [
+//                  { "v": "Date(2014, 12, 1, 22, 9, 44)", "f": null },
+//                  { "v": "0", "f": null }
+//              ]
+//        }
+//    ]
+//, "p": null
