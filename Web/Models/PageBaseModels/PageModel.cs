444525550d5a44cb27e425c17cb2048a053f725d
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2014-12-08 20:29:25 +0000

    Added version querystring to the bundled js url
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@774 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Web/Models/PageBaseModels/PageModel.cs
+++ Web/Models/PageBaseModels/PageModel.cs
@@ -18,7 +18,7 @@ namespace Web.Models.PageBaseModels
             CssUrl = BundleConfig.BundleUrl;
             Version = contextResult.Version;
             GoogleAnalyticsModel = new GoogleAnalyticsModel(contextResult);
-            JsPath = contextResult.IsInProduction ? "/_build/main.js" : "/Scripts/require.js";
+            JsPath = contextResult.IsInProduction ? string.Format("/_build/main.js?v={0}", contextResult.Version) : "/Scripts/require.js";
             JsLoaderPath = contextResult.IsInProduction ? null : "/FrontEnd/js/require.loader.dev";
         }
 
