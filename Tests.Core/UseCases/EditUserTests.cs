9d761df19382adc487c52ead0726cb27c06e47b4
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2015-02-09 21:17:21 +0000

    Added tests for EditUser
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@859 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Tests.Core/UseCases/EditUserTests.cs
+++ Tests.Core/UseCases/EditUserTests.cs
@@ -1,7 +1,68 @@
-﻿namespace Tests.Core.UseCases
+﻿using Core.Exceptions;
+using Core.UseCases.EditUser;
+using NUnit.Framework;
+using Tests.Common;
+
+namespace Tests.Core.UseCases
 {
-    public class EditUserTests
+    public class EditUserTests : TestBase
     {
-        
+        private const string ChangedDisplayName = "changeddisplayname";
+        private const string RealName = "realname";
+        private const string ChangedEmail = "email@example.com";
+
+        [Test]
+        public void EditUser_EmptyDisplayName_ThrowsException()
+        {
+            var request = new EditUserRequest(Constants.UserNameA, "", RealName, ChangedEmail);
+
+            Assert.Throws<ValidationException>(() => Sut.Execute(request));
+        }
+
+        [Test]
+        public void EditUser_EmptyEmail_ThrowsException()
+        {
+            var request = new EditUserRequest(Constants.UserNameA, ChangedDisplayName, RealName, "");
+
+            Assert.Throws<ValidationException>(() => Sut.Execute(request));
+        }
+
+        [Test]
+        public void EditUser_InvalidEmail_ThrowsException()
+        {
+            var request = new EditUserRequest(Constants.UserNameA, ChangedDisplayName, RealName, "a");
+
+            Assert.Throws<ValidationException>(() => Sut.Execute(request));
+        }
+
+        [Test]
+        public void EditUser_ValidInput_ReturnUrlIsSet()
+        {
+            var request = new EditUserRequest(Constants.UserNameA, ChangedDisplayName, RealName, ChangedEmail);
+
+            var result = Sut.Execute(request);
+
+            Assert.AreEqual("/-/user/details/user-name-a", result.ReturnUrl.Relative);
+        }
+
+        [Test]
+        public void EditUser_ValidInput_UserIsSaved()
+        {
+            var request = new EditUserRequest(Constants.UserNameA, ChangedDisplayName, RealName, ChangedEmail);
+
+            Sut.Execute(request);
+
+            Assert.AreEqual(Constants.UserNameA, Repos.User.Saved.UserName);
+            Assert.AreEqual(ChangedDisplayName, Repos.User.Saved.DisplayName);
+            Assert.AreEqual(ChangedEmail, Repos.User.Saved.Email);
+        }
+
+        private EditUserInteractor Sut
+        {
+            get
+            {
+                return new EditUserInteractor(Repos.User);
+            }
+        }
     }
 }
