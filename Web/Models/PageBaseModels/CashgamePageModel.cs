08db11a854f465f0821adc1acc9af6c45aea1079
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2015-02-17 20:51:05 +0000

    Removed the old running and start buttons
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@871 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Web/Models/PageBaseModels/CashgamePageModel.cs
+++ Web/Models/PageBaseModels/CashgamePageModel.cs
@@ -1,20 +1,15 @@
 using Core.UseCases.CashgameContext;
-using Web.Models.CashgameModels.Running;
 using Web.Models.NavigationModels;
 
 namespace Web.Models.PageBaseModels
 {
     public abstract class CashgamePageModel : BunchPageModel
     {
-        public BarModel BarModel { get; private set; }
-        public StartButtonModel StartButtonModel { get; private set; }
         public CashgamePageNavigationModel PageNavModel { get; private set; }
         public CashgameYearNavigationModel YearNavModel { get; private set; }
 
         protected CashgamePageModel(string browserTitle, CashgameContextResult cashgameContextResult) : base(browserTitle, cashgameContextResult.BunchContext)
         {
-            BarModel = cashgameContextResult.GameIsRunning ? new RunningGameBarModel(cashgameContextResult.RunningCashgameUrl.Relative) : new BarModel(cashgameContextResult.AddCashgameUrl.Relative);
-            StartButtonModel = cashgameContextResult.GameIsRunning ? new RunningGameStartButtonModel(cashgameContextResult.AddCashgameUrl.Relative) : new StartButtonModel(cashgameContextResult.AddCashgameUrl.Relative);
             PageNavModel = new CashgamePageNavigationModel(cashgameContextResult);
             YearNavModel = new CashgameYearNavigationModel(cashgameContextResult);
         }
