63c916f9a4e8db399bb09df2eb6cd710513257f0
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2015-09-29 22:20:36 +0000

    Added messages to indicate that the user has no apps yet
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@1023 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Web/Models/AppModels/List/AllAppsPageModel.cs
+++ Web/Models/AppModels/List/AllAppsPageModel.cs
@@ -8,11 +8,13 @@ namespace Web.Models.AppModels.List
     public class AllAppsPageModel : AppPageModel
     {
         public IList<AppListItemModel> AppModels { get; private set; }
+        public bool HasApps { get; private set; }
 
         public AllAppsPageModel(AppContext.Result contextResult, AppList.Result appListResult)
             : base("All Apps", contextResult)
         {
             AppModels = appListResult.Items.Select(o => new AppListItemModel(o)).ToList();
+            HasApps = appListResult.Items.Any();
         }
     }
 }
--- Web/Models/AppModels/List/UserAppsPageModel.cs
+++ Web/Models/AppModels/List/UserAppsPageModel.cs
@@ -9,12 +9,14 @@ namespace Web.Models.AppModels.List
     public class UserAppsPageModel : AppPageModel
     {
         public IList<AppListItemModel> AppModels { get; private set; }
+        public bool HasApps { get; private set; }
         public string AddUrl { get; private set; }
 
         public UserAppsPageModel(AppContext.Result contextResult, AppList.Result appListResult)
             : base("User Apps", contextResult)
         {
             AppModels = appListResult.Items.Select(o => new AppListItemModel(o)).ToList();
+            HasApps = appListResult.Items.Any();
             AddUrl = new AddAppUrl().Relative;
         }
     }
