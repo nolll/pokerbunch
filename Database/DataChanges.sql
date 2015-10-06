e7a1bb7bca1ccc204de08441576c0f7b45cfe853
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2015-10-06 15:37:14 +0000

    Data changes for locations
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@1033 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- Database/DataChanges.sql
+++ Database/DataChanges.sql
@@ -1,1 +1,11 @@
-﻿
+﻿UPDATE Location SET Name = 'Enskede' WHERE Id = 6
+UPDATE Game SET LocationId = 3 WHERE GameId = 75
+UPDATE Location SET Name = 'Hammarby sjöstad' WHERE Id = 2
+UPDATE Location SET Name = 'Hjorthagen' WHERE Id = 3
+UPDATE Location SET Name = 'Rissne' WHERE Id = 12
+UPDATE Location SET Name = 'Segeltorp' WHERE Id = 8
+UPDATE Location SET Name = 'Märsta' WHERE Id = 4
+UPDATE Location SET Name = 'Bryggargatan' WHERE Id = 5
+UPDATE Location SET Name = 'Tulohuset' WHERE Id = 7
+UPDATE Location SET Name = 'Huvudsta' WHERE Id = 11
+UPDATE Game SET LocationId = 7 WHERE LocationId = 2 AND GameId > 77
