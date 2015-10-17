db990c48e3c3caf8a016c52c52118045f4eeb8e7
Henrik <Henrik@fbe6c1e3-6fed-8744-b610-9659f20f9f49>
2015-10-17 22:15:51 +0000

    trunk version -> 8.0
    git-svn-id: http://nolll.homeip.net:8081/svn/PokerBunch/trunk@1055 fbe6c1e3-6fed-8744-b610-9659f20f9f49

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("7.17.*")]
+[assembly: AssemblyVersion("8.0.*")]
