af0e92fd0f4ffdd6b4f83a66d12d0ee5f73625b8
Henrik Severinson <henriks@gmail.com>
2016-01-10 22:55:05 +0100

    Added groups to signalr, to be able to send messages to different bunches

--- Web/FrontEnd/js/signalrtest.js
+++ Web/FrontEnd/js/signalrtest.js
@@ -2,14 +2,22 @@ define(["jquery", "signalr.hubs"],
     function ($) {
         "use strict";
 
+        var slug = 'pokerpoker';
+
         function init() {
             var socket = $.connection.runningGameHub;
             socket.client.updateClient = function (message) {
                 alert(message);
             };
             $.connection.hub.start().done(function () {
-                socket.server.dataUpdated('message from client');
+                socket.server.joinGame(slug);
             });
+            setTimeout(sendMessage, 3000);
+        }
+
+        function sendMessage() {
+            var socket = $.connection.runningGameHub;
+            socket.server.dataUpdated(slug, 'message from client');
         }
 
         return {
--- Web/RunningGameHub.cs
+++ Web/RunningGameHub.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
 namespace Web
 {
     public class RunningGameHub : Hub
     {
-        public void DataUpdated(string message)
+        public void DataUpdated(string slug, string message)
         {
             var responseMessage = $"{message}, sent from server";
-            Clients.All.updateClient(responseMessage);
+            SendMessage(slug, responseMessage);
+        }
+
+        public async Task JoinGame(string slug)
+        {
+            await Groups.Add(Context.ConnectionId, slug);
+            SendMessage(slug, Context.User.Identity.Name + " joined");
+            SendMessage("other game", "message to dev null");
+        }
+
+        private void SendMessage(string slug, string message)
+        {
+            Clients.Group(slug).updateClient(message);
         }
     }
 }
