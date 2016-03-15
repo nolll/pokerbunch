3ef760ea4eef085c7b64cf87f267b05d4ea7c9f3
Henrik Severinson <henriks@gmail.com>
2016-03-16 00:17:16 +0100

    Started cleaning up sortable-table.js, but I think I will go with vue.js for these tables instead

--- Web/FrontEnd/js/sortable-table.js
+++ Web/FrontEnd/js/sortable-table.js
@@ -4,13 +4,30 @@ define(["jquery"],
 
         function SortableTable(el) {
             var me = this;
+            me.el = el;
             me.$el = $(el);
             me.createForm();
         }
 
         SortableTable.prototype.createForm = function () {
             var me = this;
-            var $div = $("<div class='table-list--sortable__sort-order-selector'><label for='sortorder'>Select Data:</label></div>");
+            var $div = me.createDiv();
+            var $dropdown = me.createDropdown();
+            $div.append($dropdown);
+            var containerId = me.el.getAttribute("data-sort-control-container");
+            var $container = $(document.getElementById(containerId));
+            $container.prepend($div);
+            $dropdown[0].addEventListener("change", function() {
+                me.navigate(this.value);
+            });
+        };
+        
+        SortableTable.prototype.createDiv = function () {
+            return $("<div class='table-list--sortable__sort-order-selector'><label for='sortorder'>Select Data:</label></div>");
+        }
+
+        SortableTable.prototype.createDropdown = function () {
+            var me = this;
             var $dropdown = $("<select id='sortorder' name='sortorder'></select>");
             var $tableHeaders = me.$el.find("th");
             $tableHeaders.each(function () {
@@ -24,14 +41,8 @@ define(["jquery"],
                     $dropdown.append($option);
                 }
             });
-            $div.append($dropdown);
-            var containerId = me.$el.attr("data-sort-control-container");
-            var $container = $(document.getElementById(containerId));
-            $container.prepend($div);
-            $dropdown.on("change", function() {
-                me.navigate(this.value);
-            });
-        };
+            return $dropdown;
+        }
 
         SortableTable.prototype.navigate = function (url) {
             location.href = url;
