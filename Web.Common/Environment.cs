ef2e1398d1bebe4b3c7416fb1349a4d30d144ef5
Henrik Severinson <henriks@gmail.com>
2016-03-24 00:18:06 +0100

    Added the test host as an exception to force https

--- Web.Common/Environment.cs
+++ Web.Common/Environment.cs
@@ -7,6 +7,11 @@ namespace Web.Common
             return hostName.EndsWith("pokerbunch.lan");
         }
 
+        public static bool IsTest(string hostName)
+        {
+            return hostName.EndsWith("homeip.net");
+        }
+
         public static bool IsStage(string hostName)
         {
             return hostName.EndsWith("staging.pokerbunch.com");
