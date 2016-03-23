4ff41940ff8003b5be8d3769702243964529eb25
Henrik Severinson <henriks@gmail.com>
2016-03-24 00:13:05 +0100

    The route fix didn't work. Added a hint path now, that points to the nuget package

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -10,12 +10,6 @@ namespace Web
             routes.LowercaseUrls = true;
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default",
-                "{controller}/{action}/{id}",
-                new {controller = "Controller name", action = "Action name", id = UrlParameter.Optional}
-                );
-
             routes.MapRoute("Error", "{*url}",
                 new { controller = "Error", action = "NotFound" }
             );
