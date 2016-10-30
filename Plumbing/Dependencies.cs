dd0a54d01e62c2a4c760d2ff6d09b55e7b4b2876
Henrik Severinson <henriks@gmail.com>
2016-10-30 11:23:21 +0100

    Optimized Dependencies

--- Plumbing/Dependencies.cs
+++ Plumbing/Dependencies.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Repositories;
 using Core.Services;
 using Infrastructure;
@@ -10,8 +11,13 @@ namespace Plumbing
 {
     public class Dependencies
     {
-        private readonly SqlServerStorageProvider _db;
-        private readonly ApiConnection _apiConnection;
+        private readonly string _connectionString;
+        private readonly string _apiUrl;
+        private readonly string _apiKey;
+        private readonly string _apiToken;
+        private SqlServerStorageProvider _db;
+        private ApiConnection _api;
+
         private ILocationRepository _locationRepository;
         private IBunchRepository _bunchRepository;
         private AppService _appService;
@@ -26,18 +32,23 @@ namespace Plumbing
         public Dependencies(ICacheContainer cacheContainer, string connectionString, string apiUrl, string apiKey, string apiToken)
         {
             Cache = cacheContainer;
-            _db = new SqlServerStorageProvider(connectionString);
-            _apiConnection = new ApiConnection(apiUrl, apiKey, apiToken);
+            _connectionString = connectionString;
+            _apiUrl = apiUrl;
+            _apiKey = apiKey;
+            _apiToken = apiToken;
         }
 
-        public ILocationRepository LocationRepository => _locationRepository ?? (_locationRepository = new ApiLocationRepository(_apiConnection));
-        public IBunchRepository BunchRepository => _bunchRepository ?? (_bunchRepository = new ApiBunchRepository(_apiConnection));
-        public AppService AppService => _appService ?? (_appService = new AppService(new CachedAppRepository(new SqlAppRepository(_db), Cache)));
-        public CashgameService CashgameService => _cashgameService ?? (_cashgameService = new CashgameService(new CachedCashgameRepository(new SqlCashgameRepository(_db), Cache)));
-        public EventService EventService => _eventService ?? (_eventService = new EventService(new CachedEventRepository(new SqlEventRepository(_db), Cache)));
-        public PlayerService PlayerService => _playerService ?? (_playerService = new PlayerService(new CachedPlayerRepository(new SqlPlayerRepository(_db), Cache)));
-        public UserService UserService => _userService ?? (_userService = new UserService(new CachedUserRepository(new SqlUserRepository(_db), Cache)));
-        public AuthService AuthService => _authService ?? (_authService = new AuthService(new ApiTokenRepository(_apiConnection)));
+        private SqlServerStorageProvider Db => _db ?? (_db = new SqlServerStorageProvider(_connectionString));
+        private ApiConnection Api => _api ?? (_api = new ApiConnection(_apiUrl, _apiKey, _apiToken));
+
+        public ILocationRepository LocationRepository => _locationRepository ?? (_locationRepository = new ApiLocationRepository(Api));
+        public IBunchRepository BunchRepository => _bunchRepository ?? (_bunchRepository = new ApiBunchRepository(Api));
+        public AppService AppService => _appService ?? (_appService = new AppService(new CachedAppRepository(new SqlAppRepository(Db), Cache)));
+        public CashgameService CashgameService => _cashgameService ?? (_cashgameService = new CashgameService(new CachedCashgameRepository(new SqlCashgameRepository(Db), Cache)));
+        public EventService EventService => _eventService ?? (_eventService = new EventService(new CachedEventRepository(new SqlEventRepository(Db), Cache)));
+        public PlayerService PlayerService => _playerService ?? (_playerService = new PlayerService(new CachedPlayerRepository(new SqlPlayerRepository(Db), Cache)));
+        public UserService UserService => _userService ?? (_userService = new UserService(new CachedUserRepository(new SqlUserRepository(Db), Cache)));
+        public AuthService AuthService => _authService ?? (_authService = new AuthService(new ApiTokenRepository(Api)));
         public IRandomService RandomService => _randomService ?? (_randomService = new RandomService());
         public IMessageSender MessageSender => _messageSender ?? (_messageSender = new MessageSender());
         public ICacheContainer Cache { get; }
