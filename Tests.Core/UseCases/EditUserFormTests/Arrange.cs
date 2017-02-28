15c3a60436beed75fc9db2be0e2a8f10fe125d15
Henrik Severinson <henriks@gmail.com>
2017-02-28 21:14:48 +0100

    Rewrote EditUserForm

---
+++ Tests.Core/UseCases/EditUserFormTests/Arrange.cs
@@ -0,0 +1,30 @@
+using Core.Entities;
+using Core.Repositories;
+using Core.UseCases;
+using Tests.Core.Data;
+
+namespace Tests.Core.UseCases.EditUserFormTests
+{
+    public class Arrange : UseCaseTest<EditUserForm>
+    {
+        protected EditUserForm.Result Result;
+
+        private const string _userId = UserData.Id1;
+        protected const string UserName = UserData.UserName1;
+        protected const string RealName = "real-name";
+        protected const string DisplayName = UserData.DisplayName1;
+        protected const string Email = "email@example.com";
+
+        protected override void Setup()
+        {
+            var user = new User(_userId, UserName, DisplayName, RealName, Email);
+
+            Mock<IUserRepository>().Setup(o => o.GetByNameOrEmail(UserName)).Returns(user);
+        }
+
+        protected override void Execute()
+        {
+            Result = Sut.Execute(new EditUserForm.Request(UserName));
+        }
+    }
+}
