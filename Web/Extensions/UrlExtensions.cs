e98a5f2c2da7a59197dc39e7b5e993a6e1bc9831
Henrik Severinson <henriks@gmail.com>
2017-09-25 22:32:17 +0200

    Added urlextension for absolute

---
+++ Web/Extensions/UrlExtensions.cs
@@ -0,0 +1,12 @@
+﻿using PokerBunch.Common.Urls.SiteUrls;
+
+namespace Web.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string Absolute(this SiteUrl url)
+        {
+            return url.Absolute(SiteSettings.SiteHost);
+        }
+    }
+}
