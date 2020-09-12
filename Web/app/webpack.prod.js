b5942c702c18bffbc32a2e94678aaf88ef620e0d
Henrik <henriks@gmail.com>
2020-09-12 02:06:16 +0200

    added webpack analyze

--- Web/app/webpack.prod.js
+++ Web/app/webpack.prod.js
@@ -4,8 +4,20 @@ var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    plugins: [
-        new OptimizeCssAssetsPlugin()
-    ],
+    plugins: getPlugins(),
     devtool: 'source-map'
 });
+
+function getPlugins() {
+    const plugins = [
+        new OptimizeCssAssetsPlugin()
+    ];
+
+    if (process.env.ANALYZE_BUNDLE) {
+        const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+        plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    return plugins;
+}
