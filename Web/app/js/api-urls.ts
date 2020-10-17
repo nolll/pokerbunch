f06d9f09cfdad2241e565355369df432156792b0
Henrik <henriks@gmail.com>
2020-10-17 17:42:30 +0200

    password url

--- Web/app/js/api-urls.ts
+++ Web/app/js/api-urls.ts
@@ -26,7 +26,7 @@ export default {
         list: (slug: string) => `/bunches/${slug}/players`
     },
     user: {
-        changePassword: '/user/password/change',
+        password: '/user/password',
         current: '/user',
         get: (userName: string) => `/users/${userName}`,
         update: (userName: string) => `/users/${userName}`,
