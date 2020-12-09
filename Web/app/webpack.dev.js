22b9f8c1dac9899da0d2f3efa6a4b3b9b1702e1a
Henrik Severinson <henriks@gmail.com>
2020-12-09 07:49:51 +0000

    Npm update
    * updated cross-env, ts-loader, vue-loader, dayjs and vue-router
    
    * updated ts-debounce and vuex
    
    * updated axios
    
    * upgraded @babel/core, @babel-preset-env, babel-loader and core-js
    
    * upgraded webpack-merge
    
    * upgraded copy-webpack-plugin, html-webpack-plugin, less-loader and vue-property-decorator

--- Web/app/webpack.dev.js
+++ Web/app/webpack.dev.js
@@ -2,6 +2,5 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    stats: 'verbose'
+    mode: 'development'
 });
