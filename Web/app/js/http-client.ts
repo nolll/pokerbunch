75ba3be961cd76fe5d8f492b102a0e8cb5674330
Henrik <henriks@gmail.com>
2022-01-14 17:19:13 +0100

    updated axios

--- Web/app/js/http-client.ts
+++ Web/app/js/http-client.ts
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosResponseHeaders } from 'axios';
 
 export default {
-    get<T>(url: string, headers?: object): Promise<IHttpResponse<T>> {
-        return axios.get<T>(url, { headers });
+    get<T>(url: string, headers?: AxiosResponseHeaders): Promise<IHttpResponse<T>> {
+        return axios.get<T, AxiosResponse<T>>(url, { headers });
     },
-    post<T>(url: string, data?: object, headers?: object): Promise<IHttpResponse<T>> {
+    post<T>(url: string, data?: object, headers?: AxiosResponseHeaders): Promise<IHttpResponse<T>> {
         return axios.post<T>(url, data, { headers });
     },
-    put<T>(url: string, data: object, headers?: object): Promise<IHttpResponse<T>> {
+    put<T>(url: string, data: object, headers?: AxiosResponseHeaders): Promise<IHttpResponse<T>> {
         return axios.put<T>(url, data, { headers });
     },
-    delete<T>(url: string, headers?: object): Promise<IHttpResponse<T>> {
+    delete<T>(url: string, headers?: AxiosResponseHeaders): Promise<IHttpResponse<T>> {
         return axios.delete<T>(url, { headers });
     }
 };
