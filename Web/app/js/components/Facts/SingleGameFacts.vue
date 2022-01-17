08ff36ff704997f86101128790ac22a7ed134848
Henrik <henriks@gmail.com>
2022-01-17 16:41:46 +0100

    facts

--- Web/app/js/components/Facts/SingleGameFacts.vue
+++ Web/app/js/components/Facts/SingleGameFacts.vue
@@ -1,71 +1,61 @@
 ﻿<template>
-    <div v-if="ready">
-        <h2 class="h2">Single Game</h2>
-        <DefinitionList>
-            <DefinitionTerm>Best Result</DefinitionTerm>
-            <PlayerResultFact :name="facts.bestResult.name" :amount="facts.bestResult.amount" />
+  <div v-if="ready">
+    <h2 class="h2">Single Game</h2>
+    <DefinitionList>
+      <DefinitionTerm>Best Result</DefinitionTerm>
+      <PlayerResultFact :name="facts.bestResult.name" :amount="facts.bestResult.amount" />
 
-            <DefinitionTerm>Worst Result</DefinitionTerm>
-            <PlayerResultFact :name="facts.worstResult.name" :amount="facts.worstResult.amount" />
-        </DefinitionList>
-    </div>
+      <DefinitionTerm>Worst Result</DefinitionTerm>
+      <PlayerResultFact :name="facts.worstResult.name" :amount="facts.worstResult.amount" />
+    </DefinitionList>
+  </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+//Things to add
+//BiggestBuyin
+//BiggestCashout
+//BiggestComeback
 
-    //Things to add
-    //BiggestBuyin
-    //BiggestCashout
-    //BiggestComeback
+import PlayerResultFact from './PlayerResultFact.vue';
+import DefinitionList from '@/components/DefinitionList/DefinitionList.vue';
+import DefinitionTerm from '@/components/DefinitionList/DefinitionTerm.vue';
+import { CashgameListPlayerData } from '@/models/CashgameListPlayerData';
+import { SingleGameFactCollection } from '@/models/SingleGameFactCollection';
+import { PlayerWinningsFact } from '@/models/PlayerWinningsFact';
+import { computed } from 'vue';
+import useGameArchive from '@/composables/useGameArchive';
+import useBunches from '@/composables/useBunches';
 
-    import { Component, Prop, Mixins } from 'vue-property-decorator';
-    import PlayerResultFact from './PlayerResultFact.vue';
-    import DefinitionList from '@/components/DefinitionList/DefinitionList.vue';
-    import DefinitionTerm from '@/components/DefinitionList/DefinitionTerm.vue';
-    import { BunchMixin, FormatMixin, GameArchiveMixin } from '@/mixins';
-    import { CashgameListPlayerData } from '@/models/CashgameListPlayerData';
-    import { SingleGameFactCollection } from '@/models/SingleGameFactCollection';
-    import { PlayerWinningsFact } from '@/models/PlayerWinningsFact';
+const bunches = useBunches();
+const gameArchive = useGameArchive();
 
-    @Component({
-        components: {
-            PlayerResultFact,
-            DefinitionList,
-            DefinitionTerm
-        }
-    })
-    export default class SingleGameFacts extends Mixins(
-        BunchMixin,
-        FormatMixin,
-        GameArchiveMixin
-    ) {
-        get facts() {
-            return getFacts(this.$_sortedPlayers);
-        }
-        
-        get ready() {
-            return this.$_bunchReady && this.$_gamesReady;
-        }
+const facts = computed(() => {
+  return getFacts(gameArchive.sortedPlayers.value);
+});
+
+const ready = computed(() => {
+  return bunches.bunchReady.value && gameArchive.gamesReady.value;
+});
+
+const getFacts = (players: CashgameListPlayerData[]): SingleGameFactCollection => {
+  var bestResult: PlayerWinningsFact = { name: '', id: '0', amount: 0 };
+  var worstResult: PlayerWinningsFact = { name: '', id: '0', amount: 0 };
+  for (var pi = 0; pi < players.length; pi++) {
+    var player = players[pi];
+    for (var gi = 0; gi < player.gameResults.length; gi++) {
+      var game = player.gameResults[gi];
+      if (game && game.winnings > bestResult.amount) {
+        bestResult = { name: player.name, id: player.id, amount: game.winnings };
+      }
+      if (game && game.winnings < worstResult.amount) {
+        worstResult = { name: player.name, id: player.id, amount: game.winnings };
+      }
     }
-
-    function getFacts(players: CashgameListPlayerData[]): SingleGameFactCollection {
-        var bestResult: PlayerWinningsFact = { name: '', id: '0', amount: 0 };
-        var worstResult: PlayerWinningsFact = { name: '', id: '0', amount: 0 };
-        for (var pi = 0; pi < players.length; pi++) {
-            var player = players[pi];
-            for (var gi = 0; gi < player.gameResults.length; gi++) {
-                var game = player.gameResults[gi];
-                if (game && game.winnings > bestResult.amount) {
-                    bestResult = { name: player.name, id: player.id, amount: game.winnings };
-                }
-                if (game && game.winnings < worstResult.amount) {
-                    worstResult = { name: player.name, id: player.id, amount: game.winnings };
-                }
-            }
-        }
-        return {
-            bestResult: bestResult,
-            worstResult: worstResult
-        }
-    }
+  }
+  return {
+    bestResult: bestResult,
+    worstResult: worstResult,
+  };
+};
 </script>
--- Web/app/js/components/Facts/TotalFacts.vue
+++ Web/app/js/components/Facts/TotalFacts.vue
@@ -1,96 +1,81 @@
 ﻿<template>
-    <div v-if="ready">
-        <h2 class="h2">Totals</h2>
-        <DefinitionList>
-            <DefinitionTerm>Most Time Played</DefinitionTerm>
-            <PlayerTimeFact :name="facts.mostTime.name" :minutes="facts.mostTime.minutes" />
+  <div v-if="ready">
+    <h2 class="h2">Totals</h2>
+    <DefinitionList>
+      <DefinitionTerm>Most Time Played</DefinitionTerm>
+      <PlayerTimeFact :name="facts.mostTime.name" :minutes="facts.mostTime.minutes" />
 
-            <DefinitionTerm>Best Total Result</DefinitionTerm>
-            <PlayerResultFact :name="facts.bestTotal.name" :amount="facts.bestTotal.amount" />
+      <DefinitionTerm>Best Total Result</DefinitionTerm>
+      <PlayerResultFact :name="facts.bestTotal.name" :amount="facts.bestTotal.amount" />
 
-            <DefinitionTerm>Worst Total Result</DefinitionTerm>
-            <PlayerResultFact :name="facts.worstTotal.name" :amount="facts.worstTotal.amount" />
+      <DefinitionTerm>Worst Total Result</DefinitionTerm>
+      <PlayerResultFact :name="facts.worstTotal.name" :amount="facts.worstTotal.amount" />
 
-            <DefinitionTerm>Biggest Total Buyin</DefinitionTerm>
-            <PlayerResultFact :name="facts.biggestBuyin.name" :amount="facts.biggestBuyin.amount" />
+      <DefinitionTerm>Biggest Total Buyin</DefinitionTerm>
+      <PlayerResultFact :name="facts.biggestBuyin.name" :amount="facts.biggestBuyin.amount" />
 
-            <DefinitionTerm>Biggest Total Cashout</DefinitionTerm>
-            <PlayerResultFact :name="facts.biggestCashout.name" :amount="facts.biggestCashout.amount" />
-        </DefinitionList>
-    </div>
+      <DefinitionTerm>Biggest Total Cashout</DefinitionTerm>
+      <PlayerResultFact :name="facts.biggestCashout.name" :amount="facts.biggestCashout.amount" />
+    </DefinitionList>
+  </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+//Things to add
+//MostGamesPlayed
+//HighestWinrate
 
-    //Things to add
-    //MostGamesPlayed
-    //HighestWinrate
+import PlayerResultFact from './PlayerResultFact.vue';
+import PlayerTimeFact from './PlayerTimeFact.vue';
+import DefinitionList from '@/components/DefinitionList/DefinitionList.vue';
+import DefinitionTerm from '@/components/DefinitionList/DefinitionTerm.vue';
+import { CashgameListPlayerData } from '@/models/CashgameListPlayerData';
+import { TotalFactCollection } from '@/models/TotalFactCollection';
+import { computed } from 'vue';
+import useGameArchive from '@/composables/useGameArchive';
+import useBunches from '@/composables/useBunches';
 
-    import { Component, Prop, Mixins } from 'vue-property-decorator';
-    import PlayerAmountFact from './PlayerAmountFact.vue';
-    import PlayerResultFact from './PlayerResultFact.vue';
-    import PlayerTimeFact from './PlayerTimeFact.vue';
-    import DefinitionList from '@/components/DefinitionList/DefinitionList.vue';
-    import DefinitionTerm from '@/components/DefinitionList/DefinitionTerm.vue';
-    import { BunchMixin, FormatMixin, GameArchiveMixin } from '@/mixins';
-    import { CashgameListPlayerData } from '@/models/CashgameListPlayerData';
-    import { TotalFactCollection } from '@/models/TotalFactCollection';
-    
-    @Component({
-        components: {
-            PlayerAmountFact,
-            PlayerResultFact,
-            PlayerTimeFact,
-            DefinitionList,
-            DefinitionTerm
-        }
-    })
-    export default class TotalFacts extends Mixins(
-        BunchMixin,
-        FormatMixin,
-        GameArchiveMixin
-    ) {
-        @Prop() readonly text!: string;
+const bunches = useBunches();
+const gameArchive = useGameArchive();
 
-        get facts() {
-            return getFacts(this.$_sortedPlayers);
-        }
+const facts = computed(() => {
+  return getFacts(gameArchive.sortedPlayers.value);
+});
 
-        get ready() {
-            return this.$_bunchReady && this.$_sortedGames.length > 0;
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value && gameArchive.sortedGames.value.length > 0;
+});
+
+const getFacts = (players: CashgameListPlayerData[]): TotalFactCollection => {
+  var mostTime = { name: '', id: '0', minutes: 0 };
+  var bestTotal = { name: '', id: '0', amount: 0 };
+  var worstTotal = { name: '', id: '0', amount: 0 };
+  var biggestBuyin = { name: '', id: '0', amount: 0 };
+  var biggestCashout = { name: '', id: '0', amount: 0 };
+  for (var pi = 0; pi < players.length; pi++) {
+    var player = players[pi];
+    if (player.playedTimeInMinutes > mostTime.minutes) {
+      mostTime = { name: player.name, id: player.id, minutes: player.playedTimeInMinutes };
     }
-
-    function getFacts(players: CashgameListPlayerData[]): TotalFactCollection {
-        var mostTime = { name: '', id: '0', minutes: 0 };
-        var bestTotal = { name: '', id: '0', amount: 0 };
-        var worstTotal = { name: '', id: '0', amount: 0 };
-        var biggestBuyin = { name: '', id: '0', amount: 0 };
-        var biggestCashout = { name: '', id: '0', amount: 0 };
-        for (var pi = 0; pi < players.length; pi++) {
-            var player = players[pi];
-            if (player.playedTimeInMinutes > mostTime.minutes) {
-                mostTime = { name: player.name, id: player.id, minutes: player.playedTimeInMinutes };
-            }
-            if (player.winnings > bestTotal.amount) {
-                bestTotal = { name: player.name, id: player.id, amount: player.winnings };
-            }
-            if (player.winnings < worstTotal.amount) {
-                worstTotal = { name: player.name, id: player.id, amount: player.winnings };
-            }
-            if (player.buyin > biggestBuyin.amount) {
-                biggestBuyin = { name: player.name, id: player.id, amount: player.buyin };
-            }
-            if (player.stack > biggestCashout.amount) {
-                biggestCashout = { name: player.name, id: player.id, amount: player.stack };
-            }
-        }
-        return {
-            mostTime: mostTime,
-            bestTotal: bestTotal,
-            worstTotal: worstTotal,
-            biggestBuyin: biggestBuyin,
-            biggestCashout: biggestCashout
-        }
+    if (player.winnings > bestTotal.amount) {
+      bestTotal = { name: player.name, id: player.id, amount: player.winnings };
     }
+    if (player.winnings < worstTotal.amount) {
+      worstTotal = { name: player.name, id: player.id, amount: player.winnings };
+    }
+    if (player.buyin > biggestBuyin.amount) {
+      biggestBuyin = { name: player.name, id: player.id, amount: player.buyin };
+    }
+    if (player.stack > biggestCashout.amount) {
+      biggestCashout = { name: player.name, id: player.id, amount: player.stack };
+    }
+  }
+  return {
+    mostTime: mostTime,
+    bestTotal: bestTotal,
+    worstTotal: worstTotal,
+    biggestBuyin: biggestBuyin,
+    biggestCashout: biggestCashout,
+  };
+};
 </script>
