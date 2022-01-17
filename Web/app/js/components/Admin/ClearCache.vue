00a61e9faf67a836067ca9f11d5146989961c145
Henrik <henriks@gmail.com>
2022-01-17 11:11:12 +0100

    clear cache

--- Web/app/js/components/Admin/ClearCache.vue
+++ Web/app/js/components/Admin/ClearCache.vue
@@ -1,40 +1,35 @@
 <template>
-    <div>
-        <h2 class="module-heading">Clear cache</h2>
-        <p>
-            <CustomButton text="Clear" type="action" v-on:click="clearCache" />
-        </p>
-        <p v-if="hasMessage">
-            {{message}}
-        </p>
-    </div>
+  <div>
+    <h2 class="module-heading">Clear cache</h2>
+    <p>
+      <CustomButton text="Clear" :type="buttonType" v-on:click="clearCache" />
+    </p>
+    <p v-if="hasMessage">
+      {{ message }}
+    </p>
+  </div>
 </template>
 
-<script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import api from '@/api'; 
-    import CustomButton from '@/components/Common/CustomButton.vue';
+<script setup lang="ts">
+import api from '@/api';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import { ButtonType } from '@/models/ButtonType';
+import { computed, ref } from 'vue';
 
-    @Component({
-        components: {
-            CustomButton
-        }
-    })
-    export default class ClearCache extends Vue{
-        message: string | null = null;
+const message = ref<string | null>(null);
+const buttonType = ButtonType.Action;
 
-        get hasMessage(){
-            return !!this.message;
-        }
+const hasMessage = computed(() => {
+  return !!message.value;
+});
 
-        async clearCache(){
-            var response = await api.clearCache();
-            this.message = response.data.message;
-            setTimeout(this.clearMessage, 3000);
-        }
+const clearCache = async () => {
+  var response = await api.clearCache();
+  message.value = response.data.message;
+  setTimeout(clearMessage, 3000);
+};
 
-        clearMessage(){
-            this.message = null;
-        }
-    }
+const clearMessage = () => {
+  message.value = null;
+};
 </script>
