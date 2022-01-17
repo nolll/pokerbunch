c4780ec0243a8a71291d4dd740de371ad77d6914
Henrik <henriks@gmail.com>
2022-01-17 17:06:12 +0100

    matrix

--- Web/app/js/components/Matrix/MatrixItem.vue
+++ Web/app/js/components/Matrix/MatrixItem.vue
@@ -1,49 +1,43 @@
 ﻿<template>
-    <TableListCell :is-numeric="true" :custom-classes="winnerClass">
-        <template v-if="isInGame">
-            <span class="matrix__value"><WinningsText :value="winnings" :showCurrency="false" /></span>
-            <span class="matrix__transaction">in {{buyin}}</span>
-            <span class="matrix__transaction">out {{stack}}</span>
-        </template>
-    </TableListCell>
+  <TableListCell :is-numeric="true" :custom-classes="winnerClass">
+    <template v-if="isInGame">
+      <span class="matrix__value"><WinningsText :value="winnings" :showCurrency="false" /></span>
+      <span class="matrix__transaction">in {{ buyin }}</span>
+      <span class="matrix__transaction">out {{ stack }}</span>
+    </template>
+  </TableListCell>
 </template>
 
-<script lang="ts">
-    import { CashgamePlayerData } from '@/models/CashgamePlayerData';
-    import { CssClasses } from '@/models/CssClasses';
-    import { Component, Prop, Vue } from 'vue-property-decorator';
-    import TableListCell from '@/components/Common/TableList/TableListCell.vue';
-    import WinningsText from '@/components/Common/WinningsText.vue';
-    
-    @Component({
-        components: {
-            TableListCell,
-            WinningsText
-        }
-    })
-    export default class MatrixItem extends Vue {
-        @Prop() readonly game!: CashgamePlayerData;
+<script setup lang="ts">
+import { CashgamePlayerData } from '@/models/CashgamePlayerData';
+import { CssClasses } from '@/models/CssClasses';
+import TableListCell from '@/components/Common/TableList/TableListCell.vue';
+import WinningsText from '@/components/Common/WinningsText.vue';
+import { computed } from 'vue';
 
-        get buyin() {
-            return this.game.buyin;
-        }
+const props = defineProps<{
+  game: CashgamePlayerData;
+}>();
 
-        get stack() {
-            return this.game.stack;
-        }
+const buyin = computed(() => {
+  return props.game.buyin;
+});
 
-        get winnings() {
-            return this.game.winnings;
-        }
+const stack = computed(() => {
+  return props.game.stack;
+});
 
-        get winnerClass(): CssClasses {
-            return {
-                'matrix__winner': this.game.isWinner
-            };
-        }
+const winnings = computed(() => {
+  return props.game.winnings;
+});
 
-        get isInGame() {
-            return this.game.playedThisGame;
-        }
-    }
+const winnerClass = computed((): CssClasses => {
+  return {
+    matrix__winner: props.game.isWinner,
+  };
+});
+
+const isInGame = computed(() => {
+  return props.game.playedThisGame;
+});
 </script>
