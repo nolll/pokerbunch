94b942e4206d21cf9ca06dd020e02932afba3838
Henrik <henriks@gmail.com>
2022-01-17 17:52:05 +0100

    add bunch

--- Web/app/js/components/Pages/AddBunchPage.vue
+++ Web/app/js/components/Pages/AddBunchPage.vue
@@ -1,150 +1,128 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <PageSection>
-            <Block>
-                <PageHeading text="Create Bunch" />
-            </Block>
+  <Layout :ready="ready">
+    <PageSection>
+      <Block>
+        <PageHeading text="Create Bunch" />
+      </Block>
 
-            <template v-if="isSaving">
-                <Block>
-                    <p>
-                        Your bunch has been created.
-                    </p>
-                    <p>
-                        <CustomLink :url="bunchUrl">Go to bunch!</CustomLink>
-                    </p>
-                </Block>
-            </template>
-            <Block v-else>
-                <p>
-                    <label class="label" for="name">Name</label>
-                    <input class="textfield" v-model="name" id="name" type="text">
-                </p>
-                <p>
-                    <label class="label" for="description">Description</label>
-                    <input class="textfield" v-model="description" id="description" type="text">
-                </p>
-                <p>
-                    <label class="label" for="currencySymbol">Currency Symbol</label>
-                    <input class="textfield" v-model="currencySymbol" id="currencySymbol" type="text">
-                </p>
-                <p>
-                    <label class="label" for="currencyLayout">Currency Layout</label>
-                    <CurrencyLayoutDropdown v-model="currencyLayout" :symbol="currencySymbol" />
-                </p>
-                <p>
-                    <label class="label" for="timezone">Timezone</label>
-                    <TimezoneDropdown v-model="timezone" />
-                </p>
-                <p v-if="hasError" class="validation-error">
-                    {{errorMessage}}
-                </p>
-                <p>
-                    <CustomButton v-on:click="save" type="action" text="Save" />
-                    <CustomButton v-on:click="back" text="Cancel" />
-                </p>
-            </Block>
-
-        </PageSection>
-    </Layout>
+      <template v-if="isSaving">
+        <Block>
+          <p>Your bunch has been created.</p>
+          <p>
+            <CustomLink :url="bunchUrl">Go to bunch!</CustomLink>
+          </p>
+        </Block>
+      </template>
+      <Block v-else>
+        <p>
+          <label class="label" for="name">Name</label>
+          <input class="textfield" v-model="name" id="name" type="text" />
+        </p>
+        <p>
+          <label class="label" for="description">Description</label>
+          <input class="textfield" v-model="description" id="description" type="text" />
+        </p>
+        <p>
+          <label class="label" for="currencySymbol">Currency Symbol</label>
+          <input class="textfield" v-model="currencySymbol" id="currencySymbol" type="text" />
+        </p>
+        <p>
+          <label class="label" for="currencyLayout">Currency Layout</label>
+          <CurrencyLayoutDropdown v-model="currencyLayout" :symbol="currencySymbol" />
+        </p>
+        <p>
+          <label class="label" for="timezone">Timezone</label>
+          <TimezoneDropdown v-model="timezone" />
+        </p>
+        <p v-if="hasError" class="validation-error">
+          {{ errorMessage }}
+        </p>
+        <p>
+          <CustomButton v-on:click="save" type="action" text="Save" />
+          <CustomButton v-on:click="back" text="Cancel" />
+        </p>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { TimezoneMixin, UserMixin } from '@/mixins';
-    import urls from '@/urls';
-    import api from '@/api';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import CustomLink from '@/components/Common/CustomLink.vue';
-    import { User } from '@/models/User';
-    import { AxiosError } from 'axios';
-    import { ApiError} from '@/models/ApiError';
-    import { ApiParamsAddBunch } from '@/models/ApiParamsAddBunch';
-    import TimezoneDropdown from '@/components/TimezoneDropdown.vue';
-    import CurrencyLayoutDropdown from '@/components/CurrencyLayoutDropdown.vue';
-    
-    @Component({
-        components: {
-            Layout,
-            Block,
-            PageHeading,
-            PageSection,
-            CustomButton,
-            CustomLink,
-            TimezoneDropdown,
-            CurrencyLayoutDropdown
-        }
-    })
-    export default class AddBunchPage extends Mixins(
-        TimezoneMixin,
-        UserMixin
-    ) {
-        description = '';
-        name = '';
-        currencySymbol = '$';
-        currencyLayout = '';
-        timezone = '';
-        errorMessage = '';
-        isSaving = false;
-        savedSlug = '';
+<script setup lang="ts">
+import urls from '@/urls';
+import api from '@/api';
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import CustomLink from '@/components/Common/CustomLink.vue';
+import { AxiosError } from 'axios';
+import { ApiError } from '@/models/ApiError';
+import { ApiParamsAddBunch } from '@/models/ApiParamsAddBunch';
+import TimezoneDropdown from '@/components/TimezoneDropdown.vue';
+import CurrencyLayoutDropdown from '@/components/CurrencyLayoutDropdown.vue';
+import useTimezones from '@/composables/useTimezones';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
-        get hasError(){
-            return !!this.errorMessage;
-        }
+const route = useRoute();
+const timezones = useTimezones();
 
-        get loginUrl(){
-            return urls.auth.login;
-        }
+const description = ref('');
+const name = ref('');
+const currencySymbol = ref('$');
+const currencyLayout = ref('');
+const timezone = ref('');
+const errorMessage = ref('');
+const isSaving = ref(false);
+const savedSlug = ref('');
 
-        get bunchUrl(){
-            return urls.bunch.details(this.savedSlug);
-        }
+const hasError = computed(() => {
+  return !!errorMessage.value;
+});
 
-        async save(){
-            this.errorMessage = '';
+const bunchUrl = computed(() => {
+  return urls.bunch.details(savedSlug.value);
+});
 
-            try{
-                const params: ApiParamsAddBunch = {
-                    name: this.name,
-                    description: this.description,
-                    currencySymbol: this.currencySymbol,
-                    currencyLayout: this.currencyLayout,
-                    timezone: this.timezone
-                };
+const save = async () => {
+  errorMessage.value = '';
 
-                const response = await api.addBunch(params);
-                this.savedSlug = response.data.id;
-                this.isSaving = true;
-            } catch (err){
-                const error = err as AxiosError<ApiError>;
-                const message = error.response?.data.message || 'Unknown Error';
-                this.errorMessage = message;
-            }
-        }
+  try {
+    const params: ApiParamsAddBunch = {
+      name: name.value,
+      description: description.value,
+      currencySymbol: currencySymbol.value,
+      currencyLayout: currencyLayout.value,
+      timezone: timezone.value,
+    };
 
-        back(){
-            history.back();
-        }
+    const response = await api.addBunch(params);
+    savedSlug.value = response.data.id;
+    isSaving.value = true;
+  } catch (err) {
+    const error = err as AxiosError<ApiError>;
+    const message = error.response?.data.message || 'Unknown Error';
+    errorMessage.value = message;
+  }
+};
 
-        get ready(){
-            return this.$_timezonesReady;
-        }
+const back = () => {
+  history.back();
+};
 
-        init() {
-            this.$_loadTimezones();
-        }
+const ready = computed(() => {
+  return timezones.timezonesReady.value;
+});
 
-        mounted() {
-            this.init();
-        }
+const init = () => {
+  timezones.loadTimezones();
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
