c304692276feebf048af636b6627493f5fd0e291
Henrik <henriks@gmail.com>
2022-01-17 17:43:59 +0100

    overview

--- Web/app/js/components/Overview/OverviewItem.vue
+++ Web/app/js/components/Overview/OverviewItem.vue
@@ -1,39 +1,24 @@
 ﻿<template>
-    <TableListCell :is-numeric="true">
-        <span class="matrix__value" v-if="isInGame"><WinningsText :value="winnings" /></span>
-    </TableListCell>
+  <TableListCell :is-numeric="true">
+    <span class="matrix__value" v-if="isInGame"><WinningsText :value="winnings" /></span>
+  </TableListCell>
 </template>
 
-<script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
-    import { CashgamePlayerData } from '@/models/CashgamePlayerData';
-    import { CssClasses } from '@/models/CssClasses';
-    import TableListCell from '@/components/Common/TableList/TableListCell.vue';
-    import WinningsText from '@/components/Common/WinningsText.vue';
-    
-    @Component({
-        components: {
-            TableListCell,
-            WinningsText
-        }
-    })
-    export default class OverviewItem extends Vue {
-        @Prop() readonly game!: CashgamePlayerData;
+<script setup lang="ts">
+import { CashgamePlayerData } from '@/models/CashgamePlayerData';
+import TableListCell from '@/components/Common/TableList/TableListCell.vue';
+import WinningsText from '@/components/Common/WinningsText.vue';
+import { computed } from 'vue';
 
-        get buyin() {
-            return this.game.buyin;
-        }
+const props = defineProps<{
+  game: CashgamePlayerData;
+}>();
 
-        get stack() {
-            return this.game.stack;
-        }
+const winnings = computed(() => {
+  return props.game.winnings;
+});
 
-        get winnings() {
-            return this.game.winnings;
-        }
-
-        get isInGame() {
-            return this.game.playedThisGame;
-        }
-    }
+const isInGame = computed(() => {
+  return props.game.playedThisGame;
+});
 </script>
