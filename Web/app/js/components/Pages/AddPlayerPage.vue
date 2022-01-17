f61313a407ee93d31885b88412287d06ebc6bc69
Henrik <henriks@gmail.com>
2022-01-17 20:12:27 +0100

    add player

--- Web/app/js/components/Pages/AddPlayerPage.vue
+++ Web/app/js/components/Pages/AddPlayerPage.vue
@@ -1,88 +1,80 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <template slot="top-nav">
-            <BunchNavigation />
-        </template>
+  <Layout :ready="ready">
+    <template slot="top-nav">
+      <BunchNavigation />
+    </template>
 
-        <PageSection>
-            <Block>
-                <PageHeading text="Add Player" />
-            </Block>
+    <PageSection>
+      <Block>
+        <PageHeading text="Add Player" />
+      </Block>
 
-            <Block>
-                <div class="field">
-                    <label class="label" for="player-name">Name</label>
-                    <input class="textfield" v-model="playerName" ref="playerName" id="player-name" type="text">
-                </div>
-                <div class="buttons">
-                    <CustomButton v-on:click="add" type="action" text="Add" />
-                    <CustomButton v-on:click="cancel" text="Cancel" />
-                </div>
-            </Block>
-        </PageSection>
-    </Layout>
+      <Block>
+        <div class="field">
+          <label class="label" for="player-name">Name</label>
+          <input class="textfield" v-model="playerName" ref="playerName" id="player-name" type="text" />
+        </div>
+        <div class="buttons">
+          <CustomButton v-on:click="add" type="action" text="Add" />
+          <CustomButton v-on:click="cancel" text="Cancel" />
+        </div>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Prop, Watch } from 'vue-property-decorator';
-    import { BunchMixin, PlayerMixin, UserMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
-    import Block from '@/components/Common/Block.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import urls from '@/urls';
-    
-    @Component({
-        components: {
-            Layout,
-            BunchNavigation,
-            CustomButton,
-            Block,
-            PageHeading,
-            PageSection
-        }
-    })
-    export default class AddPlayerPage extends Mixins(
-        BunchMixin,
-        PlayerMixin,
-        UserMixin
-    ) {
-        playerName = '';
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
+import Block from '@/components/Common/Block.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import urls from '@/urls';
+import useBunches from '@/composables/useBunches';
+import useUsers from '@/composables/useUsers';
+import usePlayers from '@/composables/usePlayers';
+import { useRoute, useRouter } from 'vue-router';
+import { computed, onMounted, ref, watch } from 'vue';
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-            this.$_loadPlayers();
-        }
+const route = useRoute();
+const router = useRouter();
+const users = useUsers();
+const bunches = useBunches();
+const players = usePlayers();
 
-        add(){
-            if(this.playerName.length > 0){
-                this.$_addPlayer(this.playerName);
-                this.redirect();
-            }
-        }
+const playerName = ref('');
 
-        cancel(){
-            this.redirect();
-        }
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+  players.loadPlayers();
+};
 
-        redirect() {
-            this.$router.push(urls.player.list(this.$_slug));
-        }
+const add = () => {
+  if (playerName.value.length > 0) {
+    players.addPlayer(playerName.value);
+    redirect();
+  }
+};
 
-        get ready() {
-            return this.$_bunchReady && this.$_playersReady;
-        }
+const cancel = () => {
+  redirect();
+};
 
-        mounted() {
-            this.init();
-        }
+const redirect = () => {
+  router.push(urls.player.list(bunches.slug.value));
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const ready = computed(() => {
+  return bunches.bunchReady.value && players.playersReady.value;
+});
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
