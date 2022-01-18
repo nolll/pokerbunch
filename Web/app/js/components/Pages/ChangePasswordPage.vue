22b8b3a82fb509205b07e40f2fd338064d60cbc7
Henrik <henriks@gmail.com>
2022-01-18 08:35:52 +0100

    change password page

--- Web/app/js/components/Pages/ChangePasswordPage.vue
+++ Web/app/js/components/Pages/ChangePasswordPage.vue
@@ -1,124 +1,112 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <PageSection>
-            <Block>
-                <PageHeading text="Change Password" />
-            </Block>
+  <Layout :ready="ready">
+    <PageSection>
+      <Block>
+        <PageHeading text="Change Password" />
+      </Block>
 
-            <template v-if="isSaving">
-                <Block>
-                    <p>
-                        Your password was changed
-                    </p>
-                </Block>
-                <Block>
-                    <CustomButton type="action" v-on:click="back" text="Back" />
-                </Block>
-            </template>
-            <Block v-else>
-                <p>
-                    <label class="label" for="oldPassword">Old Password</label>
-                    <input class="textfield" v-model="oldPassword" id="oldPassword" type="password">
-                </p>
-                <p>
-                    <label class="label" for="newPassword">New Password</label>
-                    <input class="textfield" v-model="newPassword" id="newPassword" type="password">
-                </p>
-                <p>
-                    <label class="label" for="repeat">Repeat New Password</label>
-                    <input class="textfield" v-model="repeat" id="repeat" type="password">
-                </p>
-                <p v-if="hasError" class="validation-error">
-                    {{errorMessage}}
-                </p>
-                <p>
-                    <CustomButton v-on:click="save" type="action" text="Save" />
-                    <CustomButton v-on:click="back" text="Cancel" />
-                </p>
-            </Block>
-
-        </PageSection>
-    </Layout>
+      <template v-if="isSaving">
+        <Block>
+          <p>Your password was changed</p>
+        </Block>
+        <Block>
+          <CustomButton type="action" v-on:click="back" text="Back" />
+        </Block>
+      </template>
+      <Block v-else>
+        <p>
+          <label class="label" for="oldPassword">Old Password</label>
+          <input class="textfield" v-model="oldPassword" id="oldPassword" type="password" />
+        </p>
+        <p>
+          <label class="label" for="newPassword">New Password</label>
+          <input class="textfield" v-model="newPassword" id="newPassword" type="password" />
+        </p>
+        <p>
+          <label class="label" for="repeat">Repeat New Password</label>
+          <input class="textfield" v-model="repeat" id="repeat" type="password" />
+        </p>
+        <p v-if="hasError" class="validation-error">
+          {{ errorMessage }}
+        </p>
+        <p>
+          <CustomButton v-on:click="save" type="action" text="Save" />
+          <CustomButton v-on:click="back" text="Cancel" />
+        </p>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { UserMixin } from '@/mixins';
-    import urls from '@/urls';
-    import api from '@/api';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import { User } from '@/models/User';
-    import { AxiosError } from 'axios';
-    import { ApiError} from '@/models/ApiError';
-    import { ApiParamsChangePassword } from '@/models/ApiParamsChangePassword';
-    
-    @Component({
-        components: {
-            Layout,
-            Block,
-            PageHeading,
-            PageSection,
-            CustomButton
-        }
-    })
-    export default class ChangePasswordPage extends Mixins(
-        UserMixin
-    ) {
-        oldPassword = '';
-        newPassword = '';
-        repeat = '';
-        errorMessage = '';
-        isSaving = false;
+<script setup lang="ts">
+import urls from '@/urls';
+import api from '@/api';
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import { User } from '@/models/User';
+import { AxiosError } from 'axios';
+import { ApiError } from '@/models/ApiError';
+import { ApiParamsChangePassword } from '@/models/ApiParamsChangePassword';
+import useUsers from '@/composables/useUsers';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-        get ready() {
-            return this.$_userReady;
-        }
+const route = useRoute();
+const router = useRouter();
+const users = useUsers();
 
-        get hasError(){
-            return !!this.errorMessage;
-        }
+const oldPassword = ref('');
+const newPassword = ref('');
+const repeat = ref('');
+const errorMessage = ref('');
+const isSaving = ref(false);
 
-        async save(){
-            this.errorMessage = '';
+const ready = computed(() => {
+  return users.userReady.value;
+});
 
-            if(this.repeat !== this.newPassword){
-                this.errorMessage = 'Passwords doesn\'t match';
-                return;
-            }
+const hasError = computed(() => {
+  return !!errorMessage.value;
+});
 
-            try{
-                const params: ApiParamsChangePassword = {
-                    oldPassword: this.oldPassword,
-                    newPassword: this.newPassword
-                };
-                const response = await api.changePassword(params);
-                this.isSaving = true;
-            } catch (err){
-                const error = err as AxiosError<ApiError>;
-                const message = error.response?.data.message || 'Unknown Error';
-                this.errorMessage = message;
-            }
-        }
+const save = async () => {
+  errorMessage.value = '';
 
-        back(){
-            this.$router.push(urls.user.details(this.$_userName));
-        }
+  if (repeat.value !== newPassword.value) {
+    errorMessage.value = "Passwords doesn't match";
+    return;
+  }
 
-        init() {
-            this.$_requireUser();
-        }
+  try {
+    const params: ApiParamsChangePassword = {
+      oldPassword: oldPassword.value,
+      newPassword: newPassword.value,
+    };
+    const response = await api.changePassword(params);
+    isSaving.value = true;
+  } catch (err) {
+    const error = err as AxiosError<ApiError>;
+    const message = error.response?.data.message || 'Unknown Error';
+    errorMessage.value = message;
+  }
+};
 
-        mounted() {
-            this.init();
-        }
+const back = () => {
+  router.push(urls.user.details(users.userName.value));
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const init = () => {
+  users.requireUser();
+};
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
