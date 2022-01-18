716b3036ae0843f1e38652de25d080391bbf8e51
Henrik <henriks@gmail.com>
2022-01-18 11:36:01 +0100

    facts page

--- Web/app/js/components/Pages/FactsPage.vue
+++ Web/app/js/components/Pages/FactsPage.vue
@@ -1,82 +1,70 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <template slot="top-nav">
-            <BunchNavigation />
-        </template>
+  <Layout :ready="ready">
+    <template slot="top-nav">
+      <BunchNavigation />
+    </template>
 
-        <PageSection>
-            <Block>
-                <CashgameNavigation page="facts" />
-            </Block>
-        </PageSection>
+    <PageSection>
+      <Block>
+        <CashgameNavigation page="facts" />
+      </Block>
+    </PageSection>
 
-        <PageSection>
-            <Block>
-                <SingleGameFacts />
-            </Block>
-            <Block>
-                <TotalFacts />
-            </Block>
-            <template slot="aside2">
-                <Block>
-                    <OverallFacts />
-                </Block>
-            </template>
-        </PageSection>
+    <PageSection>
+      <Block>
+        <SingleGameFacts />
+      </Block>
+      <Block>
+        <TotalFacts />
+      </Block>
+      <template slot="aside2">
+        <Block>
+          <OverallFacts />
+        </Block>
+      </template>
+    </PageSection>
 
-        <template slot="main">
-            <PageSection>
-            </PageSection>
-        </template>
-    </Layout>
+    <template slot="main">
+      <PageSection> </PageSection>
+    </template>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { BunchMixin, UserMixin, GameArchiveMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
-    import CashgameNavigation from '@/components/Navigation/CashgameNavigation.vue';
-    import SingleGameFacts from '@/components/Facts/SingleGameFacts.vue';
-    import TotalFacts from '@/components/Facts/TotalFacts.vue';
-    import OverallFacts from '@/components/Facts/OverallFacts.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
+import CashgameNavigation from '@/components/Navigation/CashgameNavigation.vue';
+import SingleGameFacts from '@/components/Facts/SingleGameFacts.vue';
+import TotalFacts from '@/components/Facts/TotalFacts.vue';
+import OverallFacts from '@/components/Facts/OverallFacts.vue';
+import Block from '@/components/Common/Block.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import useUsers from '@/composables/useUsers';
+import useBunches from '@/composables/useBunches';
+import useGameArchive from '@/composables/useGameArchive';
+import { computed, onMounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
-    @Component({
-        components: {
-            Layout,
-            BunchNavigation,
-            CashgameNavigation,
-            SingleGameFacts,
-            TotalFacts,
-            OverallFacts,
-            Block,
-            PageSection
-        }
-    })
-    export default class FactsPage extends Mixins(
-        BunchMixin,
-        UserMixin,
-        GameArchiveMixin
-    ) {
-        get ready() {
-            return this.$_bunchReady && this.$_gamesReady;
-        }
+const route = useRoute();
+const users = useUsers();
+const bunches = useBunches();
+const gameArchive = useGameArchive();
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-            this.$_loadGames();
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value && gameArchive.gamesReady.value;
+});
 
-        mounted() {
-            this.init();
-        }
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+  gameArchive.loadGames();
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
