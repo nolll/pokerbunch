3d3b2cc45c96e47be24c21fce71696fc693f24d5
Henrik <henriks@gmail.com>
2022-01-18 11:24:44 +0100

    event details

--- Web/app/js/components/Pages/EventDetailsPage.vue
+++ Web/app/js/components/Pages/EventDetailsPage.vue
@@ -1,103 +1,88 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <template slot="top-nav">
-            <BunchNavigation />
-        </template>
+  <Layout :ready="ready">
+    <template slot="top-nav">
+      <BunchNavigation />
+    </template>
 
-        <PageSection>
-            <Block>
-                <PageHeading :text="name" />
-            </Block>
-            <Block>
-                <MatrixTable :slug="slug" :games="games" />
-            </Block>
-        </PageSection>
-    </Layout>
+    <PageSection>
+      <Block>
+        <PageHeading :text="name" />
+      </Block>
+      <Block>
+        <MatrixTable :slug="slug" :games="games" />
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { BunchMixin, EventMixin, UserMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
-    import Block from '@/components/Common/Block.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import urls from '@/urls';
-    import api from '@/api';
-    import { ArchiveCashgameResponse } from '@/response/ArchiveCashgameResponse';
-    import MatrixTable from '@/components/Matrix/MatrixTable.vue';
-    import { ArchiveCashgame } from '@/models/ArchiveCashgame';
-    
-    @Component({
-        components: {
-            Layout,
-            BunchNavigation,
-            CustomButton,
-            Block,
-            PageHeading,
-            PageSection,
-            MatrixTable
-        }
-    })
-    export default class EventDetailsPage extends Mixins(
-        BunchMixin,
-        EventMixin,
-        UserMixin
-    ) {
-        games: ArchiveCashgame[] = [];
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import api from '@/api';
+import MatrixTable from '@/components/Matrix/MatrixTable.vue';
+import { ArchiveCashgame } from '@/models/ArchiveCashgame';
+import useUsers from '@/composables/useUsers';
+import useEvents from '@/composables/useEvents';
+import useBunches from '@/composables/useBunches';
+import { useRoute } from 'vue-router';
+import { computed, onMounted, ref, watch } from 'vue';
 
-        get name() {
-            if(this.event)
-                return this.event.name;
-            return '';
-        }
+const route = useRoute();
+const users = useUsers();
+const bunches = useBunches();
+const events = useEvents();
 
-        get slug(){
-            return this.$_slug;
-        }
+const games = ref<ArchiveCashgame[]>([]);
 
-        get event() {
-            for(let i = 0; i < this.$_events.length; i++){
-                const event = this.$_events[i];
-                if(event.id.toString() === this.eventId)
-                    return event;
-            }
-            return null;
-        }
+const name = computed(() => {
+  if (event.value) return event.value.name;
+  return '';
+});
 
-        get eventId() {
-            return this.$route.params.id;
-        }
+const slug = computed(() => {
+  return bunches.slug.value;
+});
 
-        get ready() {
-            return this.$_bunchReady && this.$_eventsReady;
-        }
+const event = computed(() => {
+  for (let i = 0; i < events.events.value.length; i++) {
+    const event = events.events.value[i];
+    if (event.id.toString() === eventId.value) return event;
+  }
+  return null;
+});
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-            this.$_loadEvents();
-            this.loadGames();
-        }
+const eventId = computed(() => {
+  return route.params.id as string;
+});
 
-        async loadGames(){
-            try{
-                const response = await api.getEventGames(this.$_slug, this.eventId);
-                this.games = response.data.map(o => ArchiveCashgame.fromResponse(o));
-            } catch {
-                this.games = [];
-            }
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value && events.eventsReady.value;
+});
 
-        mounted() {
-            this.init();
-        }
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+  events.loadEvents();
+  loadGames();
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const loadGames = async () => {
+  try {
+    const response = await api.getEventGames(bunches.slug.value, eventId.value);
+    games.value = response.data.map((o) => ArchiveCashgame.fromResponse(o));
+  } catch {
+    games.value = [];
+  }
+};
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
--- Web/app/js/models/ArchiveCashgame.ts
+++ Web/app/js/models/ArchiveCashgame.ts
@@ -5,62 +5,67 @@ import { ArchiveCashgameLocation } from './ArchiveCashgameLocation';
 import { ArchiveCashgamePlayer } from './ArchiveCashgamePlayer';
 
 export class ArchiveCashgame {
-    id: string;
-    startTime: Date;
-    updatedTime: Date;
-    location: ArchiveCashgameLocation;
-    players: ArchiveCashgamePlayer[];
-    turnover: number;
-    averageBuyin: number;
-    duration: number;
+  id: string;
+  startTime: Date;
+  updatedTime: Date;
+  location: ArchiveCashgameLocation;
+  players: ArchiveCashgamePlayer[];
+  turnover: number;
+  averageBuyin: number;
+  duration: number;
 
-    constructor(id: string, startTime: Date, updatedTime: Date, location: ArchiveCashgameLocation, players: ArchiveCashgamePlayer[]) {
-        this.id = id;
-        this.startTime = startTime;
-        this.updatedTime = updatedTime;
-        this.location = location;
-        this.players = players.sort((a, b) => a.winnings - b.winnings).reverse();
-        this.turnover = this.getTurnover();
-        this.averageBuyin = this.getAverageBuyin();
-        this.duration = timeFunctions.diffInMinutes(startTime, updatedTime)
+  constructor(
+    id: string,
+    startTime: Date,
+    updatedTime: Date,
+    location: ArchiveCashgameLocation,
+    players: ArchiveCashgamePlayer[]
+  ) {
+    this.id = id;
+    this.startTime = startTime;
+    this.updatedTime = updatedTime;
+    this.location = location;
+    this.players = players.sort((a, b) => a.winnings - b.winnings).reverse();
+    this.turnover = this.getTurnover();
+    this.averageBuyin = this.getAverageBuyin();
+    this.duration = timeFunctions.diffInMinutes(startTime, updatedTime);
+  }
+
+  public get date() {
+    return this.startTime;
+  }
+
+  public get playerCount() {
+    return this.players.length;
+  }
+
+  public isBestPlayer(playerId: string) {
+    if (this.players.length === 0) return false;
+
+    return this.players[0].id === playerId;
+  }
+
+  public getAverageBuyin() {
+    const sum = this.getTurnover();
+    const playerCount = this.players.length;
+    return Math.round(sum / playerCount);
+  }
+
+  public getTurnover() {
+    let sum = 0;
+    for (const p of this.players) {
+      sum += p.buyin;
     }
+    return sum;
+  }
 
-    public get date(){
-        return this.startTime;
-    }
-
-    public get playerCount(){
-        return this.players.length;
-    }
-
-    public isBestPlayer(playerId: string){
-        if(this.players.length === 0)
-            return false;
-
-        return this.players[0].id === playerId;
-    }
-
-    private getAverageBuyin() {
-        const sum = this.getTurnover();
-        const playerCount = this.players.length;
-        return Math.round(sum / playerCount);
-    }
-
-    private getTurnover() {
-        let sum = 0;
-        for (const p of this.players) {
-            sum += p.buyin;
-        }
-        return sum;
-    }
-
-    public static fromResponse(response: ArchiveCashgameResponse){
-        return new ArchiveCashgame(
-            response.id,
-            dayjs(response.startTime).toDate(),
-            dayjs(response.updatedTime).toDate(),
-            ArchiveCashgameLocation.fromResponse(response.location),
-            response.results?.map(o => ArchiveCashgamePlayer.fromResponse(o)) || []
-        );
-    }
+  public static fromResponse(response: ArchiveCashgameResponse) {
+    return new ArchiveCashgame(
+      response.id,
+      dayjs(response.startTime).toDate(),
+      dayjs(response.updatedTime).toDate(),
+      ArchiveCashgameLocation.fromResponse(response.location),
+      response.results?.map((o) => ArchiveCashgamePlayer.fromResponse(o)) || []
+    );
+  }
 }
