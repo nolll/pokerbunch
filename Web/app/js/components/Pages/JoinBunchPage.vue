032dd420fb363acb18b4763992fed21f51d50563
Henrik <henriks@gmail.com>
2022-01-18 14:03:09 +0100

    join bunch page

--- Web/app/js/components/Pages/JoinBunchPage.vue
+++ Web/app/js/components/Pages/JoinBunchPage.vue
@@ -1,115 +1,104 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <PageSection>
-            <Block>
-                <PageHeading text="Join Bunch" />
-            </Block>
+  <Layout :ready="ready">
+    <PageSection>
+      <Block>
+        <PageHeading text="Join Bunch" />
+      </Block>
 
-            <Block>
-                <p>
-                    Please enter your invitation code below to join the bunch {{bunchName}}
-                </p>
-            </Block>
-            <Block v-if="errorMessage">
-                <p class="validation-error">
-                    {{errorMessage}}
-                </p>
-            </Block>
-            <Block>
-                <div class="field">
-                    <label class="label" for="invitationCode">Invitation Code</label>
-                    <input class="longfield" v-model="code" id="invitationCode" type="text">
-                </div>
-                <div class="buttons">
-                    <CustomButton @click="joinClicked" type="action" text="Join" />
-                    <CustomButton @click="cancel" text="Cancel" />
-                </div>
-            </Block>
-        </PageSection>
-    </Layout>
+      <Block>
+        <p>Please enter your invitation code below to join the bunch {{ bunchName }}</p>
+      </Block>
+      <Block v-if="errorMessage">
+        <p class="validation-error">
+          {{ errorMessage }}
+        </p>
+      </Block>
+      <Block>
+        <div class="field">
+          <label class="label" for="invitationCode">Invitation Code</label>
+          <input class="longfield" v-model="code" id="invitationCode" type="text" />
+        </div>
+        <div class="buttons">
+          <CustomButton @click="joinClicked" type="action" text="Join" />
+          <CustomButton @click="cancel" text="Cancel" />
+        </div>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { BunchMixin, PlayerMixin, UserMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import urls from '@/urls';
-    import api from '@/api';
-    import { ApiError } from '@/models/ApiError';
-    import { AxiosError } from 'axios';
-    
-    @Component({
-        components: {
-            Layout,
-            CustomButton,
-            Block,
-            PageHeading,
-            PageSection
-        }
-    })
-    export default class JoinBunchPage extends Mixins(
-        BunchMixin,
-        PlayerMixin,
-        UserMixin
-    ) {
-        code = '';
-        errorMessage: string | null = null;
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import urls from '@/urls';
+import api from '@/api';
+import { ApiError } from '@/models/ApiError';
+import { AxiosError } from 'axios';
+import useUsers from '@/composables/useUsers';
+import useBunches from '@/composables/useBunches';
+import usePlayers from '@/composables/usePlayers';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-        get routeCode(): string | undefined{
-            return this.$route.params.code;
-        }
+const route = useRoute();
+const router = useRouter();
+const users = useUsers();
+const bunches = useBunches();
+const players = usePlayers();
 
-        get bunchName(){
-            return this.$_bunchName;
-        }
+const code = ref('');
+const errorMessage = ref<string>();
 
-        get ready() {
-            return this.$_bunchReady;
-        }
+const routeCode = computed((): string | undefined => {
+  return route.params.code as string;
+});
 
-        joinClicked(){
-            this.join(this.$_slug, this.code);
-        }
+const bunchName = computed(() => {
+  return bunches.bunchName.value;
+});
 
-        async join(bunchId: string, code: string){
-            if(code.length > 0){
-                try{
-                    await api.joinBunch(bunchId, { code });
-                    this.$_loadPlayers();
-                    this.$router.push(urls.bunch.details(this.$_slug));
-                } catch (err){
-                    const error = err as AxiosError<ApiError>;
-                    this.errorMessage = error.response?.data.message || 'Unknown Error';
-                }
-            }
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value;
+});
 
-        cancel(){
-            this.$router.push(urls.home);
-        }
+const joinClicked = () => {
+  join(bunches.slug.value, code.value);
+};
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-        }
-
-        mounted() {
-            this.init();
-        }
-
-        @Watch('ready')
-        readyChanged() {
-            if(this.ready && this.routeCode)
-                this.join(this.$_slug, this.routeCode)
-        }
-
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
+const join = async (bunchId: string, code: string) => {
+  if (code.length > 0) {
+    try {
+      await api.joinBunch(bunchId, { code });
+      players.loadPlayers();
+      router.push(urls.bunch.details(bunches.slug.value));
+    } catch (err) {
+      const error = err as AxiosError<ApiError>;
+      errorMessage.value = error.response?.data.message || 'Unknown Error';
     }
+  }
+};
+
+const cancel = () => {
+  router.push(urls.home);
+};
+
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+};
+
+watch(ready, () => {
+  if (ready.value && routeCode.value) join(bunches.slug.value, routeCode.value);
+});
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
