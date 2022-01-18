44695ca88d969ce3b4f6c9eee2725b628a7f5246
Henrik <henriks@gmail.com>
2022-01-18 10:53:56 +0100

    not found

--- Web/app/js/components/Pages/NotFoundPage.vue
+++ Web/app/js/components/Pages/NotFoundPage.vue
@@ -1,42 +1,27 @@
 ﻿<template>
-    <Layout :ready="true">
-        <PageSection>
-            <Block>
-                <PageHeading text="Page not found" />
-            </Block>
-            <Block>
-                <p>
-                    Please check the url for errors
-                </p>
-                <p>
-                    Go to <CustomLink :url="homeUrl"></CustomLink>
-                </p>
-            </Block>
-        </PageSection>
-    </Layout>
+  <Layout :ready="true">
+    <PageSection>
+      <Block>
+        <PageHeading text="Page not found" />
+      </Block>
+      <Block>
+        <p>Please check the url for errors</p>
+        <p>Go to <CustomLink :url="homeUrl"></CustomLink></p>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import urls from '@/urls';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import CustomLink from '@/components/Common/CustomLink.vue';
-    import { Component, Prop, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import urls from '@/urls';
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import CustomLink from '@/components/Common/CustomLink.vue';
+import { computed } from 'vue';
 
-    @Component({
-        components: {
-            Layout,
-            Block,
-            PageHeading,
-            PageSection,
-            CustomLink
-        }
-    })
-    export default class NotFoundPage extends Vue {
-        get homeUrl(){
-            return urls.home;
-        }
-    }
+const homeUrl = computed(() => {
+  return urls.home;
+});
 </script>
