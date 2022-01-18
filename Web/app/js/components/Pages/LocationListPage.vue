5cb3a48c4a21a3d55534e7047209d7bab12dbe17
Henrik <henriks@gmail.com>
2022-01-18 14:40:16 +0100

    location list page

--- Web/app/js/components/Pages/LocationListPage.vue
+++ Web/app/js/components/Pages/LocationListPage.vue
@@ -1,77 +1,66 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <template slot="top-nav">
-            <BunchNavigation />
-        </template>
+  <Layout :ready="ready">
+    <template slot="top-nav">
+      <BunchNavigation />
+    </template>
 
-        <PageSection>
-            <template slot="aside1">
-                <Block>
-                    <CustomButton :url="addLocationUrl" type="action" text="Add location" />
-                </Block>
-            </template>
+    <PageSection>
+      <template slot="aside1">
+        <Block>
+          <CustomButton :url="addLocationUrl" type="action" text="Add location" />
+        </Block>
+      </template>
 
-            <Block>
-                <PageHeading text="Locations" />
-            </Block>
+      <Block>
+        <PageHeading text="Locations" />
+      </Block>
 
-            <Block>
-                <LocationList />
-            </Block>
-        </PageSection>
-    </Layout>
+      <Block>
+        <LocationList />
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { BunchMixin, LocationMixin, UserMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
-    import LocationList from '@/components/LocationList/LocationList.vue';
-    import Block from '@/components/Common/Block.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import urls from '@/urls';
-    
-    @Component({
-        components: {
-            Layout,
-            BunchNavigation,
-            LocationList,
-            CustomButton,
-            Block,
-            PageHeading,
-            PageSection
-        }
-    })
-    export default class LocationListPage extends Mixins(
-        BunchMixin,
-        LocationMixin,
-        UserMixin
-    ) {
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
+import LocationList from '@/components/LocationList/LocationList.vue';
+import Block from '@/components/Common/Block.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import urls from '@/urls';
+import { useRoute } from 'vue-router';
+import useLocations from '@/composables/useLocations';
+import useBunches from '@/composables/useBunches';
+import useUsers from '@/composables/useUsers';
+import { computed, onMounted, watch } from 'vue';
 
-        get addLocationUrl() {
-            return urls.location.add(this.$_slug);
-        }
+const route = useRoute();
+const users = useUsers();
+const bunches = useBunches();
+const locations = useLocations();
 
-        get ready() {
-            return this.$_bunchReady && this.$_locationsReady;
-        }
+const addLocationUrl = computed(() => {
+  return urls.location.add(bunches.slug.value);
+});
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-            this.$_loadLocations();
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value && locations.locationsReady.value;
+});
 
-        mounted() {
-            this.init();
-        }
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+  locations.loadLocations();
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
