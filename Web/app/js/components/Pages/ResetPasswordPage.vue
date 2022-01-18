bf5070ed04ceaa874786d5853d908af3e5c82b31
Henrik <henriks@gmail.com>
2022-01-18 16:12:53 +0100

    reset password page

--- Web/app/js/components/Pages/ResetPasswordPage.vue
+++ Web/app/js/components/Pages/ResetPasswordPage.vue
@@ -1,108 +1,91 @@
 ﻿<template>
-    <Layout :ready="true">
-        <PageSection>
-            <Block>
-                <PageHeading text="Reset Password" />
-            </Block>
+  <Layout :ready="true">
+    <PageSection>
+      <Block>
+        <PageHeading text="Reset Password" />
+      </Block>
 
-            <template v-if="isSaving">
-                <Block>
-                    <p>
-                        An email was sent.
-                    </p>
-                </Block>
-                <Block>
-                    <CustomButton type="action" :url="loginUrl" text="Sign in" />
-                </Block>
-            </template>
-            <Block v-else>
-                <p>
-                    Enter your email address to reset your password.
-                </p>
-                <p>
-                    <label class="label" for="email">Email</label>
-                    <input class="textfield" v-model="email" id="email" type="text">
-                </p>
-                <p v-if="hasError" class="validation-error">
-                    {{errorMessage}}
-                </p>
-                <p>
-                    <CustomButton v-on:click="send" type="action" text="Send" />
-                    <CustomButton v-on:click="back" text="Cancel" />
-                </p>
-            </Block>
-
-        </PageSection>
-    </Layout>
+      <template v-if="isSaving">
+        <Block>
+          <p>An email was sent.</p>
+        </Block>
+        <Block>
+          <CustomButton type="action" :url="loginUrl" text="Sign in" />
+        </Block>
+      </template>
+      <Block v-else>
+        <p>Enter your email address to reset your password.</p>
+        <p>
+          <label class="label" for="email">Email</label>
+          <input class="textfield" v-model="email" id="email" type="text" />
+        </p>
+        <p v-if="hasError" class="validation-error">
+          {{ errorMessage }}
+        </p>
+        <p>
+          <CustomButton v-on:click="send" type="action" text="Send" />
+          <CustomButton v-on:click="back" text="Cancel" />
+        </p>
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Vue, Watch } from 'vue-property-decorator';
-    import { UserMixin } from '@/mixins';
-    import urls from '@/urls';
-    import api from '@/api';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import { User } from '@/models/User';
-    import { AxiosError } from 'axios';
-    import { ApiError} from '@/models/ApiError';
-    import { ApiParamsResetPassword } from '@/models/ApiParamsResetPassword';
-    
-    @Component({
-        components: {
-            Layout,
-            Block,
-            PageHeading,
-            PageSection,
-            CustomButton
-        }
-    })
-    export default class ResetPasswordPage extends Vue {
-        email = '';
-        errorMessage = '';
-        isSaving = false;
+<script setup lang="ts">
+import urls from '@/urls';
+import api from '@/api';
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import { AxiosError } from 'axios';
+import { ApiError } from '@/models/ApiError';
+import { ApiParamsResetPassword } from '@/models/ApiParamsResetPassword';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
-        get hasError(){
-            return !!this.errorMessage;
-        }
+const route = useRoute();
 
-        get loginUrl(){
-            return urls.auth.login;
-        }
+const email = ref('');
+const errorMessage = ref('');
+const isSaving = ref(false);
 
-        async send(){
-            this.errorMessage = '';
+const hasError = computed(() => {
+  return !!errorMessage.value;
+});
 
-            try{
-                const params: ApiParamsResetPassword = {
-                    email: this.email
-                };
-                const response = await api.resetPassword(params);
-                this.isSaving = true;
-            } catch (err){
-                const error = err as AxiosError<ApiError>;
-                const message = error.response?.data.message || 'Unknown Error';
-                this.errorMessage = message;
-            }
-        }
+const loginUrl = computed(() => {
+  return urls.auth.login;
+});
 
-        back(){
-            history.back();
-        }
+const send = async () => {
+  errorMessage.value = '';
 
-        init() {
-        }
+  try {
+    const params: ApiParamsResetPassword = {
+      email: email.value,
+    };
+    const response = await api.resetPassword(params);
+    isSaving.value = true;
+  } catch (err) {
+    const error = err as AxiosError<ApiError>;
+    const message = error.response?.data.message || 'Unknown Error';
+    errorMessage.value = message;
+  }
+};
 
-        mounted() {
-            this.init();
-        }
+const back = () => {
+  history.back();
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const init = () => {};
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init;
+});
 </script>
