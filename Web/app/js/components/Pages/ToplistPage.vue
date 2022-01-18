2aee8a0e1850ad1b36ce1f0a7656eda80f8c2598
Henrik <henriks@gmail.com>
2022-01-18 16:19:34 +0100

    toplist page

--- Web/app/js/components/Pages/ToplistPage.vue
+++ Web/app/js/components/Pages/ToplistPage.vue
@@ -1,66 +1,57 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <template slot="top-nav">
-            <BunchNavigation />
-        </template>
+  <Layout :ready="ready">
+    <template slot="top-nav">
+      <BunchNavigation />
+    </template>
 
-        <PageSection>
-            <Block>
-                <CashgameNavigation page="toplist" />
-            </Block>
-            <Block>
-                <TopListTable :bunchId="bunchId" />
-            </Block>
-        </PageSection>
-    </Layout>
+    <PageSection>
+      <Block>
+        <CashgameNavigation page="toplist" />
+      </Block>
+      <Block>
+        <TopListTable :bunchId="bunchId" />
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { BunchMixin, UserMixin, GameArchiveMixin } from '@/mixins';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
-    import CashgameNavigation from '@/components/Navigation/CashgameNavigation.vue';
-    import TopListTable from '@/components/TopList/TopListTable.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    
-    @Component({
-        components: {
-            Layout,
-            BunchNavigation,
-            CashgameNavigation,
-            TopListTable,
-            Block,
-            PageSection
-        }
-    })
-    export default class ToplistPage extends Mixins(
-        BunchMixin,
-        UserMixin,
-        GameArchiveMixin
-    ) {
-        get bunchId(){
-            return this.$_slug;
-        }
+<script setup lang="ts">
+import Layout from '@/components/Layouts/Layout.vue';
+import BunchNavigation from '@/components/Navigation/BunchNavigation.vue';
+import CashgameNavigation from '@/components/Navigation/CashgameNavigation.vue';
+import TopListTable from '@/components/TopList/TopListTable.vue';
+import Block from '@/components/Common/Block.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import { computed, onMounted, watch } from 'vue';
+import useBunches from '@/composables/useBunches';
+import useGameArchive from '@/composables/useGameArchive';
+import useUsers from '@/composables/useUsers';
+import { useRoute } from 'vue-router';
 
-        get ready() {
-            return this.$_bunchReady && this.$_gamesReady;
-        }
+const route = useRoute();
+const users = useUsers();
+const bunches = useBunches();
+const gameArchive = useGameArchive();
 
-        init() {
-            this.$_requireUser();
-            this.$_loadBunch();
-            this.$_loadGames();
-        }
+const bunchId = computed(() => {
+  return bunches.slug.value;
+});
 
-        mounted() {
-            this.init();
-        }
+const ready = computed(() => {
+  return bunches.bunchReady.value && gameArchive.gamesReady.value;
+});
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const init = () => {
+  users.requireUser();
+  bunches.loadBunch();
+  gameArchive.loadGames();
+};
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
