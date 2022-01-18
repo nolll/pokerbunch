2298ba862dd52cf1783e23995b3a1451ddfbc681
Henrik <henriks@gmail.com>
2022-01-18 11:13:59 +0100

    user details

--- Web/app/js/components/Pages/UserDetailsPage.vue
+++ Web/app/js/components/Pages/UserDetailsPage.vue
@@ -1,185 +1,169 @@
 ﻿<template>
-    <Layout :ready="ready">
-        <PageSection>
-            <Block>
-                <PageHeading :text="userName" />
-            </Block>
+  <Layout :ready="ready">
+    <PageSection>
+      <Block>
+        <PageHeading :text="userName" />
+      </Block>
 
-            <template slot="aside1">
-                <Block>
-                    <img :src="avatarUrl" alt="User avatar">
-                </Block>
-            </template>
+      <template slot="aside1">
+        <Block>
+          <img :src="avatarUrl" alt="User avatar" />
+        </Block>
+      </template>
 
-            <Block v-if="isEditing">
-                <p>
-                    <label class="label" for="display-name">Display Name</label>
-                    <input class="textfield" v-model="displayName" id="display-name" type="text">
-                </p>
-                <p>
-                    <label class="label" for="real-name">Real Name</label>
-                    <input class="textfield" v-model="realName" id="real-name" type="text">
-                </p>
-                <p>
-                    <label class="label" for="email">Email</label>
-                    <input class="textfield" v-model="email" id="email" type="text">
-                </p>
-                <p v-if="hasError" class="validation-error">
-                    {{errorMessage}}
-                </p>
-                <p>
-                    <CustomButton v-on:click="save" type="action" text="Save" />
-                    <CustomButton v-on:click="cancel" text="Cancel" />
-                </p>
-            </Block>
-            <Block v-else>
-                <ValueList>
-                    <ValueListKey>Display Name</ValueListKey>
-                    <ValueListValue>{{displayName}}</ValueListValue>
-                    <template v-if="canEdit">
-                        <ValueListKey>Real Name</ValueListKey>
-                        <ValueListValue>{{realName}}</ValueListValue>
-                        <ValueListKey>Email</ValueListKey>
-                        <ValueListValue>{{email}}</ValueListValue>
-                    </template>
-                </ValueList>
-            </Block>
+      <Block v-if="isEditing">
+        <p>
+          <label class="label" for="display-name">Display Name</label>
+          <input class="textfield" v-model="displayName" id="display-name" type="text" />
+        </p>
+        <p>
+          <label class="label" for="real-name">Real Name</label>
+          <input class="textfield" v-model="realName" id="real-name" type="text" />
+        </p>
+        <p>
+          <label class="label" for="email">Email</label>
+          <input class="textfield" v-model="email" id="email" type="text" />
+        </p>
+        <p v-if="hasError" class="validation-error">
+          {{ errorMessage }}
+        </p>
+        <p>
+          <CustomButton v-on:click="save" type="action" text="Save" />
+          <CustomButton v-on:click="cancel" text="Cancel" />
+        </p>
+      </Block>
+      <Block v-else>
+        <ValueList>
+          <ValueListKey>Display Name</ValueListKey>
+          <ValueListValue>{{ displayName }}</ValueListValue>
+          <template v-if="canEdit">
+            <ValueListKey>Real Name</ValueListKey>
+            <ValueListValue>{{ realName }}</ValueListValue>
+            <ValueListKey>Email</ValueListKey>
+            <ValueListValue>{{ email }}</ValueListValue>
+          </template>
+        </ValueList>
+      </Block>
 
-            <Block v-if="canEdit && !isEditing">
-                <CustomButton type="action" v-on:click="edit" text="Edit" />
-                <CustomButton type="action" url="/user/changepassword" text="Change Password" v-if="canChangePassword" />
-            </Block>
-
-        </PageSection>
-    </Layout>
+      <Block v-if="canEdit && !isEditing">
+        <CustomButton type="action" v-on:click="edit" text="Edit" />
+        <CustomButton type="action" url="/user/changepassword" text="Change Password" v-if="canChangePassword" />
+      </Block>
+    </PageSection>
+  </Layout>
 </template>
 
-<script lang="ts">
-    import { Component, Mixins, Watch } from 'vue-property-decorator';
-    import { UserMixin } from '@/mixins';
-    import urls from '@/urls';
-    import api from '@/api';
-    import Layout from '@/components/Layouts/Layout.vue';
-    import Block from '@/components/Common/Block.vue';
-    import PageHeading from '@/components/Common/PageHeading.vue';
-    import PageSection from '@/components/Common/PageSection.vue';
-    import CustomButton from '@/components/Common/CustomButton.vue';
-    import { User } from '@/models/User';
-    import ValueList from '@/components/Common/ValueList/ValueList.vue';
-    import ValueListKey from '@/components/Common/ValueList/ValueListKey.vue';
-    import ValueListValue from '@/components/Common/ValueList/ValueListValue.vue';
-    import { AxiosError } from 'axios';
-    import { ApiError} from '@/models/ApiError';
-    
-    @Component({
-        components: {
-            Layout,
-            Block,
-            PageHeading,
-            PageSection,
-            CustomButton,
-            ValueList,
-            ValueListKey,
-            ValueListValue
-        }
-    })
-    export default class UserDetailsPage extends Mixins(
-        UserMixin
-    ) {
-        user: User | null = null;
-        userReady = false;
-        displayName = '';
-        realName = '';
-        email = '';
-        isEditing = false;
-        errorMessage = '';
+<script setup lang="ts">
+import api from '@/api';
+import Layout from '@/components/Layouts/Layout.vue';
+import Block from '@/components/Common/Block.vue';
+import PageHeading from '@/components/Common/PageHeading.vue';
+import PageSection from '@/components/Common/PageSection.vue';
+import CustomButton from '@/components/Common/CustomButton.vue';
+import { User } from '@/models/User';
+import ValueList from '@/components/Common/ValueList/ValueList.vue';
+import ValueListKey from '@/components/Common/ValueList/ValueListKey.vue';
+import ValueListValue from '@/components/Common/ValueList/ValueListValue.vue';
+import { AxiosError } from 'axios';
+import { ApiError } from '@/models/ApiError';
+import useUsers from '@/composables/useUsers';
+import { useRoute } from 'vue-router';
+import { computed, onMounted, ref, watch } from 'vue';
 
-        get canEdit() {
-            return this.$_isAdmin || this.isCurrentUser;
-        }
+const route = useRoute();
+const users = useUsers();
 
-        get isCurrentUser(){
-            return this.$_userName == this.user?.userName;
-        }
+const user = ref<User>();
+const userReady = ref(false);
+const displayName = ref('');
+const realName = ref('');
+const email = ref('');
+const isEditing = ref(false);
+const errorMessage = ref('');
 
-        get canChangePassword(){
-            return this.isCurrentUser;
-        }
+const canEdit = computed(() => {
+  return users.isAdmin.value || isCurrentUser.value;
+});
 
-        get ready() {
-            return this.$_userReady && this.userReady;
-        }
+const isCurrentUser = computed(() => {
+  return users.userName.value == user.value?.userName;
+});
 
-        get userName(){
-            return this.user?.userName;
-        }
+const canChangePassword = computed(() => {
+  return isCurrentUser.value;
+});
 
-        get avatarUrl(){
-            return this.user?.avatar;
-        }
+const ready = computed(() => {
+  return users.userReady.value && userReady.value;
+});
 
-        get hasError(){
-            return !!this.errorMessage;
-        }
+const userName = computed(() => {
+  return user.value?.userName ?? '';
+});
 
-        async loadUser(){
-            const response = await api.getUser(this.$route.params.userName);
-            this.user = response.data;
-            if(this.user){
-                this.setMembers(this.user);
-            }
-            this.userReady = true;
-        }
+const avatarUrl = computed(() => {
+  return user.value?.avatar;
+});
 
-        async save(){
-            this.errorMessage = '';
+const hasError = computed(() => {
+  return !!errorMessage.value;
+});
 
-            if(!this.user){
-                this.isEditing = false;
-                return;
-            }
+const loadUser = async () => {
+  const response = await api.getUser(route.params.userName as string);
+  user.value = response.data;
+  if (user.value) {
+    setMembers(user.value);
+  }
+  userReady.value = true;
+};
 
-            this.user.displayName = this.displayName;
-            this.user.realName = this.realName;
-            this.user.email = this.email;
-            try{
-                const response = await api.updateUser(this.user);
-                this.isEditing = false;
-            } catch (err){
-                const error = err as AxiosError<ApiError>;
-                const message = error.response?.data.message || 'Unknown Error';
-                this.errorMessage = message;
-            }
-        }
+const save = async () => {
+  errorMessage.value = '';
 
-        cancel(){
-            if(this.user)
-                this.setMembers(this.user);
-            this.isEditing = false;
-        }
+  if (!user.value) {
+    isEditing.value = false;
+    return;
+  }
 
-        setMembers(user: User){
-            this.displayName = user.displayName;
-            this.realName = user.realName || '';
-            this.email = user.email || '';
-        }
+  user.value.displayName = displayName.value;
+  user.value.realName = realName.value;
+  user.value.email = email.value;
+  try {
+    const response = await api.updateUser(user.value);
+    isEditing.value = false;
+  } catch (err) {
+    const error = err as AxiosError<ApiError>;
+    const message = error.response?.data.message || 'Unknown Error';
+    errorMessage.value = message;
+  }
+};
 
-        edit(){
-            this.isEditing = true;
-        }
+const cancel = () => {
+  if (user.value) setMembers(user.value);
+  isEditing.value = false;
+};
 
-        init() {
-            this.$_requireUser();
-            this.loadUser();
-        }
+const setMembers = (user: User) => {
+  displayName.value = user.displayName;
+  realName.value = user.realName || '';
+  email.value = user.email || '';
+};
 
-        mounted() {
-            this.init();
-        }
+const edit = () => {
+  isEditing.value = true;
+};
 
-        @Watch('$route')
-        routeChanged() {
-            this.init();
-        }
-    }
+const init = () => {
+  users.requireUser();
+  loadUser();
+};
+
+onMounted(() => {
+  init();
+});
+
+watch(route, () => {
+  init();
+});
 </script>
