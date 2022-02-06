5412bf26b925d3cffe51948004aa1ea828b49992
Henrik <henriks@gmail.com>
2022-02-06 22:46:03 +0100

    reverted cashgame forms

--- Web/app/js/components/CurrentGame/CashoutForm.vue
+++ Web/app/js/components/CurrentGame/CashoutForm.vue
@@ -2,7 +2,7 @@
   <div class="form">
     <div class="field">
       <label class="label" for="cashout-stack">Stack Size</label>
-      <input class="numberfield" v-model.number="stack" v-on:focus="focus" id="cashout-stack" type="text" pattern="[0-9]*" />
+      <input class="numberfield" v-model="stack" v-on:focus="focus" id="cashout-stack" type="text" pattern="[0-9]*" />
     </div>
     <div class="buttons">
       <CustomButton v-on:click="cashout" type="action" text="Cash Out" />
