a992e1769ae5d738b56d1e0afe5e6670a1ef1c8f
Henrik <henriks@gmail.com>
2022-05-08 16:42:10 +0200

    removed getters and actions on timezone store

--- Web/app/js/store/helpers/TimezoneStoreHelpers.ts
+++ Web/app/js/store/helpers/TimezoneStoreHelpers.ts
@@ -1,19 +1,10 @@
 ﻿import { Timezone } from '@/models/Timezone';
 
-export enum TimezoneStoreGetters {
-    Timezones = 'timezone_timezones',
-    TimezonesReady = 'timezone_timezonesReady'
-}
-
-export enum TimezoneStoreActions {
-    LoadTimezones = 'timezone_loadTimezones'
-}
-
 export enum TimezoneStoreMutations {
-    SetTimezonesData = 'timezone_setTimezonesData',
+  SetTimezonesData = 'timezone_setTimezonesData',
 }
 
 export interface TimezoneStoreState {
-    _timezones: Timezone[];
-    _timezonesReady: boolean;
+  _timezones: Timezone[];
+  _timezonesReady: boolean;
 }
