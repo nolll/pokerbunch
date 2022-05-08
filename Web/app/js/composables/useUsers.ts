6a643dbf4f0da8b66afe5ed03a58bacced3edb4b
Henrik <henriks@gmail.com>
2022-05-08 16:50:16 +0200

    removed getters and actions on user store

--- Web/app/js/composables/useUsers.ts
+++ Web/app/js/composables/useUsers.ts
@@ -1,9 +1,12 @@
 import { computed, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import { UserStoreActions, UserStoreGetters } from '@/store/helpers/UserStoreHelpers';
+import { UserStoreMutations } from '@/store/helpers/UserStoreHelpers';
 import { User } from '@/models/User';
 import urls from '@/urls';
+import roles from '@/roles';
+import auth from '@/auth';
+import api from '@/api';
 
 export default function useUsers() {
   const store = useStore();
@@ -12,45 +15,65 @@ export default function useUsers() {
   const isUserRequired = ref(true);
 
   const userReady = computed((): boolean => {
-    return store.getters[UserStoreGetters.UserReady];
+    return store.state.user._userReady;
   });
 
   const isSignedIn = computed((): boolean => {
-    return store.getters[UserStoreGetters.IsSignedIn];
+    return store.state.user._isSignedIn;
   });
 
   const usersReady = computed((): boolean => {
-    return store.getters[UserStoreGetters.UsersReady];
+    return store.state.user._usersReady;
   });
 
   const userName = computed((): string => {
-    return store.getters[UserStoreGetters.UserName];
+    return store.state.user._userName;
   });
 
   const displayName = computed((): string => {
-    return store.getters[UserStoreGetters.DisplayName];
+    return store.state.user._displayName;
   });
 
   const isAdmin = computed((): boolean => {
-    return store.getters[UserStoreGetters.IsAdmin];
+    return store.state.user._role === roles.admin;
   });
 
   const users = computed((): User[] => {
-    return store.getters[UserStoreGetters.Users];
+    return store.state.user._users;
   });
 
-  const requireUser = () => {
+  const requireUser = async () => {
     isUserRequired.value = true;
-    store.dispatch(UserStoreActions.LoadCurrentUser);
+    await loadCurrentUser();
   };
 
-  const loadCurrentUser = () => {
+  const loadCurrentUser = async () => {
     isUserRequired.value = false;
-    store.dispatch(UserStoreActions.LoadCurrentUser);
+    if (!store.state.user._userInitialized) {
+      const isLoggedIn = auth.isLoggedIn();
+      store.commit(UserStoreMutations.SetUserInitialized);
+      store.commit(UserStoreMutations.SetIsSignedIn, isLoggedIn);
+      if (isLoggedIn) {
+        try {
+          const response = await api.getCurrentUser();
+          store.commit(UserStoreMutations.SetUser, response.data);
+        } catch (err) {
+          store.commit(UserStoreMutations.SetUserError);
+        }
+      } else {
+        store.commit(UserStoreMutations.SetUserError);
+      }
+    }
   };
 
-  const loadUsers = () => {
-    store.dispatch(UserStoreActions.LoadUsers);
+  const loadUsers = async () => {
+    try {
+      const response = await api.getUsers();
+      store.commit(UserStoreMutations.SetUsers, response.data);
+    } catch (error) {
+      store.commit(UserStoreMutations.SetUsersError);
+    }
+    store.commit(UserStoreMutations.SetUsersReady, true);
   };
 
   watch(userReady, (isUserReady: boolean) => {
--- Web/app/js/store/UserStore.ts
+++ Web/app/js/store/UserStore.ts
@@ -1,84 +1,45 @@
 ﻿import { StoreOptions } from 'vuex';
-import api from '@/api';
 import roles from '@/roles';
-import auth from '@/auth';
-import { UserStoreGetters, UserStoreActions, UserStoreMutations, UserStoreState } from '@/store/helpers/UserStoreHelpers';
+import { UserStoreMutations, UserStoreState } from '@/store/helpers/UserStoreHelpers';
 import { User } from '@/models/User';
 import { Role } from '@/models/Role';
 
 export default {
-    namespaced: false,
-    state: {
-        _isSignedIn: false,
-        _userName: '',
-        _displayName: '',
-        _role: roles.none,
-        _userReady: false,
-        _userInitialized: false,
-        _users: [],
-        _usersReady: false
+  namespaced: false,
+  state: {
+    _isSignedIn: false,
+    _userName: '',
+    _displayName: '',
+    _role: roles.none,
+    _userReady: false,
+    _userInitialized: false,
+    _users: [],
+    _usersReady: false,
+  },
+  mutations: {
+    [UserStoreMutations.SetIsSignedIn](state, isSignedIn) {
+      state._isSignedIn = isSignedIn;
     },
-    getters: {
-        [UserStoreGetters.IsSignedIn]: state => state._isSignedIn,
-        [UserStoreGetters.IsAdmin]: state => state._role === roles.admin,
-        [UserStoreGetters.UserName]: state => state._userName,
-        [UserStoreGetters.DisplayName]: state => state._displayName,
-        [UserStoreGetters.UserReady]: state => state._userReady,
-        [UserStoreGetters.Users]: state => state._users,
-        [UserStoreGetters.UsersReady]: state => state._usersReady
+    [UserStoreMutations.SetUser](state, user: User) {
+      state._userName = user.userName;
+      state._displayName = user.displayName;
+      state._role = user.role;
+      state._userReady = true;
     },
-    actions: {
-        async [UserStoreActions.LoadCurrentUser](context) {
-            if (!context.state._userInitialized) {
-                const isLoggedIn = auth.isLoggedIn();
-                context.commit(UserStoreMutations.SetUserInitialized);
-                context.commit(UserStoreMutations.SetIsSignedIn, isLoggedIn);
-                if (isLoggedIn) {
-                    try{
-                        const response = await api.getCurrentUser();
-                        context.commit(UserStoreMutations.SetUser, response.data);
-                    } catch(err) {
-                        context.commit(UserStoreMutations.SetUserError);
-                    }
-                } else {
-                    context.commit(UserStoreMutations.SetUserError);
-                }
-            }
-        },
-        async [UserStoreActions.LoadUsers](context) {
-            try{
-                const response = await api.getUsers();
-                context.commit(UserStoreMutations.SetUsers, response.data);
-            } catch(error){
-                context.commit(UserStoreMutations.SetUsersError);
-            }
-            context.commit(UserStoreMutations.SetUsersReady, true);
-        }
+    [UserStoreMutations.SetUserError](state) {
+      state._userName = '';
+      state._displayName = '';
+      state._role = Role.None;
+      state._userReady = true;
     },
-    mutations: {
-        [UserStoreMutations.SetIsSignedIn](state, isSignedIn) {
-            state._isSignedIn = isSignedIn;
-        },
-        [UserStoreMutations.SetUser](state, user: User) {
-            state._userName = user.userName;
-            state._displayName = user.displayName;
-            state._role = user.role;
-            state._userReady = true;
-        },
-        [UserStoreMutations.SetUserError](state) {
-            state._userName = '';
-            state._displayName = '';
-            state._role = Role.None;
-            state._userReady = true;
-        },
-        [UserStoreMutations.SetUserInitialized](state) {
-            state._userInitialized = true;
-        },
-        [UserStoreMutations.SetUsers](state, users: User[]) {
-            state._users = users;
-        },
-        [UserStoreMutations.SetUsersReady](state, isReady: boolean) {
-            state._usersReady = isReady;
-        }
-    }
+    [UserStoreMutations.SetUserInitialized](state) {
+      state._userInitialized = true;
+    },
+    [UserStoreMutations.SetUsers](state, users: User[]) {
+      state._users = users;
+    },
+    [UserStoreMutations.SetUsersReady](state, isReady: boolean) {
+      state._usersReady = isReady;
+    },
+  },
 } as StoreOptions<UserStoreState>;
