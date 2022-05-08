d388c623a4f24773feca43fbf3e8c2aaf1a0138c
Henrik <henriks@gmail.com>
2022-05-08 16:37:25 +0200

    removed getters and actions on player store

--- Web/app/js/composables/usePlayers.ts
+++ Web/app/js/composables/usePlayers.ts
@@ -1,35 +1,68 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 import { useRoute } from 'vue-router';
-import { PlayerStoreActions, PlayerStoreGetters } from '@/store/helpers/PlayerStoreHelpers';
+import { PlayerStoreMutations } from '@/store/helpers/PlayerStoreHelpers';
 import { Player } from '@/models/Player';
+import api from '@/api';
+import { PlayerResponse } from '@/response/PlayerResponse';
 
 export default function usePlayers() {
   const store = useStore();
   const route = useRoute();
 
   const playersReady = computed((): boolean => {
-    return store.getters[PlayerStoreGetters.PlayersReady];
+    return store.state.players._playersReady;
   });
 
   const players = computed((): Player[] => {
-    return store.getters[PlayerStoreGetters.Players];
+    return store.state.player._players;
   });
 
   const getPlayer = (id: string): Player => {
-    return store.getters[PlayerStoreGetters.GetPlayer](id);
+    if (!store.state.player._players) throw new Error('players not loaded');
+    let i;
+    for (i = 0; i < store.state.player._players.length; i++) {
+      if (store.state.player._players[i].id === id) {
+        return store.state.player._players[i];
+      }
+    }
+    throw new Error(`player not found: ${id}`);
   };
 
-  const loadPlayers = () => {
-    store.dispatch(PlayerStoreActions.LoadPlayers, { slug: route.params.slug });
+  const loadPlayers = async () => {
+    const slug = route.params.slug as string;
+    if (slug !== store.state.state.player._slug) {
+      store.commit(PlayerStoreMutations.SetSlug, slug);
+      const response = await api.getPlayers(slug);
+      const players = response.data.map((o) => mapPlayer(o));
+      store.commit(PlayerStoreMutations.SetPlayersData, players);
+    }
   };
 
-  const addPlayer = (name: string) => {
-    store.dispatch(PlayerStoreActions.AddPlayer, { bunchId: route.params.slug, name });
+  const addPlayer = async (name: string) => {
+    const slug = route.params.slug as string;
+    if (store.state.player._playersReady) {
+      const response = await api.addPlayer(slug, { name: name });
+      const player = mapPlayer(response.data);
+      store.commit(PlayerStoreMutations.AddPlayer, player);
+    }
   };
 
-  const deletePlayer = (player: Player) => {
-    store.dispatch(PlayerStoreActions.DeletePlayer, { player });
+  const deletePlayer = async (player: Player) => {
+    if (store.state.player._playersReady) {
+      store.commit(PlayerStoreMutations.DeletePlayer, player);
+      await api.deletePlayer(player.id);
+    }
+  };
+
+  const mapPlayer = (response: PlayerResponse): Player => {
+    return {
+      id: response.id.toString(),
+      name: response.name,
+      color: response.color,
+      userId: response.userId,
+      userName: response.userName,
+    };
   };
 
   return {
--- Web/app/js/store/helpers/PlayerStoreHelpers.ts
+++ Web/app/js/store/helpers/PlayerStoreHelpers.ts
@@ -1,36 +1,14 @@
 ﻿import { Player } from '@/models/Player';
 
-export enum PlayerStoreGetters {
-    Slug = 'player_slug',
-    Players = 'player_players',
-    PlayersReady = 'player_playersReady',
-    GetPlayer = 'player_getPlayer'
-}
-
-export enum PlayerStoreActions {
-    LoadPlayers = 'player_loadPlayers',
-    AddPlayer = 'player_addPlayer',
-    DeletePlayer = 'player_deletePlayer'
-}
-
 export enum PlayerStoreMutations {
-    SetPlayersData = 'player_setPlayersData',
-    SetSlug = 'player_setSlug',
-    AddPlayer = 'player_addPlayer',
-    DeletePlayer = 'player_deletePlayer'
+  SetPlayersData = 'player_setPlayersData',
+  SetSlug = 'player_setSlug',
+  AddPlayer = 'player_addPlayer',
+  DeletePlayer = 'player_deletePlayer',
 }
 
 export interface PlayerStoreState {
-    _slug: string;
-    _players: Player[];
-    _playersReady: boolean;
-}
-
-export interface AddPlayerParams{
-    bunchId: string;
-    name: string;
-}
-
-export interface DeletePlayerParams{
-    player: Player;
+  _slug: string;
+  _players: Player[];
+  _playersReady: boolean;
 }
