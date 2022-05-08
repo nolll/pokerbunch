fb9136280b31a11fa9339ea9a0e3c6ffe9288a50
Henrik <henriks@gmail.com>
2022-05-08 16:24:35 +0200

    removed getters and actions on location store

--- Web/app/js/composables/useLocations.ts
+++ Web/app/js/composables/useLocations.ts
@@ -1,31 +1,41 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 import { useRoute } from 'vue-router';
-import { LocationStoreActions, LocationStoreGetters } from '@/store/helpers/LocationStoreHelpers';
+import { LocationStoreMutations } from '@/store/helpers/LocationStoreHelpers';
 import { LocationResponse } from '@/response/LocationResponse';
+import api from '@/api';
 
 export default function useLocations() {
   const store = useStore();
   const route = useRoute();
 
   const locationsReady = computed((): boolean => {
-    return store.getters[LocationStoreGetters.LocationsReady];
+    return store.state.location._locationsReady;
   });
 
   const locations = computed((): LocationResponse[] => {
-    return store.getters[LocationStoreGetters.Locations];
+    return store.state.location._locations;
   });
 
   const getLocation = (id: string): LocationResponse | null => {
     return locations.value.find((l) => l.id.toString() === id) || null;
   };
 
-  const loadLocations = () => {
-    store.dispatch(LocationStoreActions.LoadLocations, { slug: route.params.slug });
+  const loadLocations = async () => {
+    const slug = route.params.slug as string;
+    if (slug !== store.state.location._slug) {
+      store.commit(LocationStoreMutations.SetSlug, slug);
+      const response = await api.getLocations(slug);
+      store.commit(LocationStoreMutations.SetLocationsData, response.data);
+    }
   };
 
-  const addLocation = (name: string) => {
-    store.dispatch(LocationStoreActions.AddLocation, { bunchId: route.params.slug, name });
+  const addLocation = async (name: string) => {
+    if (store.state.location._locationsReady) {
+      const slug = route.params.slug as string;
+      const response = await api.addLocation(slug, { name: name });
+      store.commit(LocationStoreMutations.AddLocation, response.data);
+    }
   };
 
   return {
--- Web/app/js/store/LocationStore.ts
+++ Web/app/js/store/LocationStore.ts
@@ -1,45 +1,24 @@
 ﻿import { StoreOptions } from 'vuex';
-import api from '@/api';
-import { LocationStoreGetters, LocationStoreActions, LocationStoreMutations, LocationStoreState, AddLocationParams } from '@/store/helpers/LocationStoreHelpers';
+import { LocationStoreMutations, LocationStoreState } from '@/store/helpers/LocationStoreHelpers';
 import { LocationResponse } from '@/response/LocationResponse';
 
 export default {
-    namespaced: false,
-    state: {
-        _slug: '',
-        _locations: [],
-        _locationsReady: false
+  namespaced: false,
+  state: {
+    _slug: '',
+    _locations: [],
+    _locationsReady: false,
+  },
+  mutations: {
+    [LocationStoreMutations.SetLocationsData](state, players: LocationResponse[]) {
+      state._locations = players;
+      state._locationsReady = true;
     },
-    getters: {
-        [LocationStoreGetters.Slug]: state => state._slug,
-        [LocationStoreGetters.Locations]: state => state._locations,
-        [LocationStoreGetters.LocationsReady]: state => state._locationsReady
+    [LocationStoreMutations.SetSlug](state, slug: string) {
+      state._slug = slug;
     },
-    actions: {
-        async [LocationStoreActions.LoadLocations](context, data) {
-            if (data.slug !== context.state._slug) {
-                context.commit(LocationStoreMutations.SetSlug, data.slug);
-                const response = await api.getLocations(data.slug);
-                context.commit(LocationStoreMutations.SetLocationsData, response.data);
-            }
-        },
-        async [LocationStoreActions.AddLocation](context, data: AddLocationParams) {
-            if (context.state._locationsReady) {
-                const response = await api.addLocation(data.bunchId, { name: data.name });
-                context.commit(LocationStoreMutations.AddLocation, response.data);
-            }
-        }
+    [LocationStoreMutations.AddLocation](state, location) {
+      state._locations.push(location);
     },
-    mutations: {
-        [LocationStoreMutations.SetLocationsData](state, players: LocationResponse[]) {
-            state._locations = players;
-            state._locationsReady = true;
-        },
-        [LocationStoreMutations.SetSlug](state, slug:string) {
-            state._slug = slug;
-        },
-        [LocationStoreMutations.AddLocation](state, location) {
-            state._locations.push(location);
-        }
-    }
+  },
 } as StoreOptions<LocationStoreState>;
