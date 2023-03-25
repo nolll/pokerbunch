dc216d8d6fd729286d2e8919f304fdb7455cc5d0
Henrik <henriks@gmail.com>
2023-03-25 18:40:06 +0100

    Removed Game Archive store

--- js/store/RootStore.ts
+++ js/store/RootStore.ts
@@ -3,7 +3,6 @@ import { RootStoreState } from './helpers/RootStoreHelpers';
 import BunchStore from './BunchStore';
 import CurrentGameStore from './CurrentGameStore';
 import EventStore from './EventStore';
-import GameArchiveStore from './GameArchiveStore';
 import PlayerStore from './PlayerStore';
 import UserStore from './UserStore';
 
@@ -13,7 +12,6 @@ export default {
     bunch: BunchStore,
     currentGame: CurrentGameStore,
     event: EventStore,
-    gameArchive: GameArchiveStore,
     player: PlayerStore,
     user: UserStore,
   },
