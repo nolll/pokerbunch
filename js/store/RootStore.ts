5837cc0156750a0bf0ef521a1c9cfea1e0981743
Henrik <henriks@gmail.com>
2023-03-29 21:27:22 +0200

    Removed current game store

--- js/store/RootStore.ts
+++ js/store/RootStore.ts
@@ -1,7 +1,6 @@
 ﻿import { StoreOptions } from 'vuex';
 import { RootStoreState } from './helpers/RootStoreHelpers';
 import BunchStore from './BunchStore';
-import CurrentGameStore from './CurrentGameStore';
 import PlayerStore from './PlayerStore';
 import UserStore from './UserStore';
 
@@ -9,7 +8,6 @@ export default {
   strict: true,
   modules: {
     bunch: BunchStore,
-    currentGame: CurrentGameStore,
     player: PlayerStore,
     user: UserStore,
   },
