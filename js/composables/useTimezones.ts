8a880eaa7f9365f16cbe3d50c4f096b1190063f0
Henrik <henriks@gmail.com>
2024-02-19 23:22:17 +0100

    Removed unnecessary casts

--- js/composables/useTimezones.ts
+++ js/composables/useTimezones.ts
@@ -4,8 +4,8 @@ export default function useTimezones() {
   const fallbackTimezone = '';
 
   const getTimezones = (): Timezone[] => {
-    const ids = (Intl as any).supportedValuesOf('timeZone') as string[];
-    return ids.map((id: string) => {
+    const ids = Intl.supportedValuesOf('timeZone');
+    return ids.map((id) => {
       return {
         id: id,
         name: id,
