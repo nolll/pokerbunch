3708c46902dfc4811c6661abcd7a614e6b39e342
Henrik S <henriks@gmail.com>
2025-09-28 10:51:18 +0200

    Switched to Vite
    * Switched to Vite
    
    * Removed webpack
    
    * Removed last bits of webpack

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+
+function getProxy() {
+  return {
+    '/api': {
+      //target: 'https://api.pokerbunch.com',
+      target: 'https://localhost:5010',
+      rewrite: (path) => path.replace(/^\/api/, ''),
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+    },
+  };
+}
+
+export default {
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './js'),
+    },
+  },
+  plugins: [vue()],
+  server: {
+    port: 9010,
+    proxy: getProxy(),
+  },
+};
