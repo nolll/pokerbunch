554713a195cb09d8c8cdc6692f5484b6b1e0cf97
Henrik Severinson <henriks@gmail.com>
2026-02-02 20:25:52 +0100

    Moved devserver api url to env

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
 
-function getProxy() {
+export default defineConfig(({ mode }) => { 
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-    '/api': {
-      //target: 'https://api.pokerbunch.com',
-      target: 'https://localhost:5010',
-      rewrite: (path) => path.replace(/^\/api/, ''),
-      changeOrigin: true,
-      secure: false,
-      ws: true,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './js'),
+      },
+    },
+    plugins: [vue()],
+    server: {
+      port: 9010,
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
     },
   };
-}
-
-export default {
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './js'),
-    },
-  },
-  plugins: [vue()],
-  server: {
-    port: 9010,
-    proxy: getProxy(),
-  },
-};
+});
