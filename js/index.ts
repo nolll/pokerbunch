367a7345d78280ddd7ec34c84b0c8c8252147aeb
Henrik Severinson <henriks@gmail.com>
2026-03-05 15:23:58 +0100

    Re-added icons and csp hashes

--- js/index.ts
+++ js/index.ts
@@ -5,6 +5,28 @@ import { createRouter } from 'vue-router';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import routes from './routes';
 import Root from './components/Root.vue';
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import {
+  IoCashOutline,
+  IoArrowForwardCircleOutline,
+  IoMenuOutline,
+  IoTimeOutline,
+  IoCheckmarkOutline,
+  IoTrashOutline,
+  IoCreateOutline,
+  IoCloseOutline,
+} from 'oh-vue-icons/icons';
+
+addIcons(
+  IoCashOutline,
+  IoArrowForwardCircleOutline,
+  IoMenuOutline,
+  IoTimeOutline,
+  IoCheckmarkOutline,
+  IoTrashOutline,
+  IoCreateOutline,
+  IoCloseOutline,
+);
 
 if (!browser.isCapable()) {
   alert('PokerBunch requires a better browser');
@@ -12,6 +34,7 @@ if (!browser.isCapable()) {
 
 const router = createRouter(routes);
 const app = createApp(Root);
+app.component('v-icon', OhVueIcon);
 app.use(router);
 app.use(VueQueryPlugin);
 app.mount('#app');
